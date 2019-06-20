68b43a19b7302cab7c9c15e094967fe02a5976c9
Danny Thorpe <danny@looker.com>
2019-06-19 23:43:23 -0700

    First cut of type resolver handles intrinsic types, reference types, and arrays of both resolves types of all properties of types

--- sdkGenerator.spec.ts
+++ sdkGenerator.spec.ts
@@ -1,42 +1,50 @@
-import * as Models from "./sdkModels";
-import { SdkGenerator } from "./sdkGenerator";
+import { LookerApi } from "./sdkModels";
+// import { SdkGenerator } from "./sdkGenerator";
 
 describe('ts template test', () => {
-  it ('outputs source with interpolations', () => {
-    const model: Models.ILookerApi = {
-      methods: [
-        {
-          httpMethod: "GET",
-          endpoint: "queries/{id}/run",
-          operationId: "run_query",
-          type: { name: "string" },
-          params: [
-            {
-              name: "query_id",
-              type: { name: "integer"},
-            },
-            {
-              name: "limit",
-              type: { name: "integer"},
-            },
-          ],
-        },
-        {
-          httpMethod: "POST",
-          endpoint: "queries/run/inline",
-          operationId: "run_inline_query",
-          type: { name: "string" },
-          params: [
-            {
-              name: "query",
-              type: { name: "CreateQuery" },
-            },
-          ],
-        }]
-    }
+  // it ('outputs source with interpolations', () => {
+  //   const model: Models.LookerApi = {
+  //     methods: [
+  //       {
+  //         httpMethod: "GET",
+  //         endpoint: "queries/{id}/run",
+  //         operationId: "run_query",
+  //         type: { name: "string" },
+  //         params: [
+  //           {
+  //             name: "query_id",
+  //             type: { name: "integer"},
+  //           },
+  //           {
+  //             name: "limit",
+  //             type: { name: "integer"},
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         httpMethod: "POST",
+  //         endpoint: "queries/run/inline",
+  //         operationId: "run_inline_query",
+  //         type: { name: "string" },
+  //         params: [
+  //           {
+  //             name: "query",
+  //             type: { name: "CreateQuery" },
+  //           },
+  //         ],
+  //       }]
+  //   }
+  //
+  //   const result = new SdkGenerator(model).render()
+  //
+  //   console.log(result)
+  // })
 
-    const result = new SdkGenerator(model).render()
+  it('resolves OAS schemas into types', () => {
+    const apiData = LookerApi.load('./Looker.3.1.oas.json')
+    expect(typeof apiData.types['ValidationError'].elementType).toEqual('undefined')
 
-    console.log(result)
+    const test = apiData.types.ValidationError.properties.errors.type.elementType
+    expect(test && test.name).toEqual('ValidationErrorDetail')
   })
 })
--- sdkModels.ts
+++ sdkModels.ts
@@ -1,21 +1,29 @@
-// import { ParameterObject, PathsObject, OperationObject } from "openapi3-ts"
+import * as OAS from "openapi3-ts"
+import * as fs from "fs";
+import {utf8} from "./utils";
 
 export interface IModel {}
 
-export interface IType extends IModel {
-  name: string
-}
-
-export interface IParameter extends IModel {
+export interface ISymbol {
   name: string
   type: IType
 }
 
-export interface IMethod extends IModel {
+export interface IType {
+  name?: string
+  properties: Record<string, IProperty>
+  status: string
+  elementType?: IType
+}
+
+export interface IParameter extends ISymbol {
+}
+
+export interface IMethod extends ISymbol {
   operationId: string
   httpMethod: string
   endpoint: string
-  type: IType           // function result type
+  type: IType
 
   description?: string
   params?: IParameter[]
@@ -27,7 +35,154 @@ export interface IMethod extends IModel {
   cookieArgs?: string[]
 }
 
-export interface ILookerApi extends IModel {
-  methods: Array<IMethod>;
+export interface IProperty extends ISymbol {
+  nullable: boolean
+  description: string
+  readOnly: boolean
+  writeOnly: boolean
+  deprecated: boolean
+}
+
+export interface ISymbolTable {
+  methods: Record<string, IMethod>
+  types: Record<string, IType>
+
+  resolveType(schema: OAS.SchemaObject): IType
+}
+
+class Symbol implements ISymbol {
+  name: string
+  type: IType
+  constructor (name: string, type: IType) {
+    this.name = name
+    this.type = type
+  }
+}
+
+class Property extends Symbol implements IProperty {
+  private schema: OAS.SchemaObject
+  required: boolean = false
+
+  constructor (name: string, type: IType, schema: OAS.SchemaObject) {
+    super(name, type)
+    this.schema = schema
+  }
+
+  get nullable(): boolean {
+    return this.schema.nullable || this.schema['x-looker-nullable'] || true
+  }
+
+  get description(): string {
+    return this.schema.description || ''
+  }
+
+  get readOnly(): boolean {
+    return this.schema.readOnly || false
+  }
+
+  get writeOnly(): boolean {
+    return this.schema.writeOnly || false
+  }
+
+  get deprecated(): boolean {
+    return this.schema.deprecated || this.schema['x-looker-deprecated'] || false
+  }
+}
+
+class Type implements IType {
+  readonly name?: string
+  readonly schema: OAS.SchemaObject
+  readonly properties: Record<string, IProperty> = {}
+
+  constructor (schema: OAS.SchemaObject, name?: string) {
+    this.schema = schema
+    this.name = name
+  }
+
+  get status(): string {
+    return this.schema['x-looker-status'] || ''
+  }
+
+  load(symbols: ISymbolTable): void {
+    Object.entries(this.schema.properties || {}).forEach(([propName, propSchema]) => {
+      this.properties[propName] = new Property(propName, symbols.resolveType(propSchema), propSchema)
+    })
+  }
+}
+
+class ArrayType extends Type{
+  elementType: IType
+
+  constructor(elementType: IType, schema: OAS.SchemaObject) {
+    super(schema)
+    this.elementType = elementType
+  }
+}
+
+class IntrinsicType extends Type {
+  constructor (name: string) {
+    super({}, name)
+  }
+}
+
+export class LookerApi implements ISymbolTable {
+  methods: Record<string, IMethod> = {}
+  types: Record<string, IType> = {}
+  refs: Record<string, IType> = {}
+
+  constructor () {
+    this.types['string'] = new IntrinsicType('string')
+    this.types['integer'] = new IntrinsicType('integer')
+    this.types['int64'] = new IntrinsicType('int64')
+    this.types['boolean'] = new IntrinsicType('boolean')
+    this.types['object'] = new IntrinsicType('object')
+    this.types['uri'] = new IntrinsicType('uri')
+    this.types['float'] = new IntrinsicType('float')
+    this.types['double'] = new IntrinsicType('double')
+  }
+
+  static load(specFile: string): LookerApi {
+    const specContent = fs.readFileSync(specFile, utf8)
+    const json = JSON.parse(specContent)
+    const spec = new OAS.OpenApiBuilder(json).getSpec()
+    return new LookerApi().load(spec)
+  }
+
+  load(spec: OAS.OpenAPIObject) : this {
+    if (spec.components && spec.components.schemas) {
+      Object.entries(spec.components.schemas).forEach(([name, schema]) => {
+        const t = new Type(schema, name)
+        // types[n] and corresponding refs[ref] MUST reference the same type instance!
+        this.types[name] = t
+        this.refs[`#/components/schemas/${name}`] = t
+      })
+      Object.entries(spec.components.schemas).forEach(([name, _]) => {
+        (this.resolveType(name) as Type).load(this)
+      })
+    }
+    return this
+  }
+
+  resolveType(schema: string | OAS.SchemaObject | OAS.ReferenceObject): IType {
+    if (typeof schema === 'string') {
+      return this.types[schema]
+    } else if (OAS.isReferenceObject(schema)) {
+      return this.refs[schema.$ref]
+    } else if (schema.type) {
+      if (schema.type === 'integer' && schema.format === 'int64') {
+        return this.types['int64']
+      }
+      if (schema.type === 'number' && schema.format) {
+        return this.types[schema.format]
+      }
+      if (schema.type === 'array' && schema.items) {
+        return new ArrayType(this.resolveType(schema.items), schema)
+      }
+      if (this.types[schema.type]) {
+        return this.types[schema.type]
+      }
+    }
+    throw new Error("Schema must have a ref or a type")
+  }
 }
 
