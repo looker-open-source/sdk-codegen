fe3f0037fd246351f26f8cc307bbc5afaa670b84
jax <jax@looker.com>
2019-06-28 18:11:54 +0100

    modify LookerSDK to inherit from SDKBase
    Opted to not reference the methods in SDKBase using super but rely on Phyton's MRO

--- python.fmt.ts
+++ python.fmt.ts
@@ -48,51 +48,11 @@ from typing import *
 from models import *
 from rtl.api_settings import ApiSettings
 from rtl.user_session import UserSession
+from rtl.sdk_base import SDKBase
 # from rtl.sdk_error import SDKError
 # from rtl.sdk_utils import *
 
-def LookerSDK(object):
-
-  def __init__(
-    self,
-    settings: ApiSettings = None,
-    file_name: str = "looker.ini",
-    user_session: UserSession = None,
-    base_url: str = None,
-    client_id: str = None,
-    client_secret: str = None,
-    api_version: str = None):
-    # cascade through settings initialization options
-    self.settings: ApiSettings = ApiSettings(file_name) if settings is None else settings
-    if api_version is not None:
-        self.settings.api_version = api_version
-    if base_url is not None:
-        self.settings.base_url = base_url
-    if client_id is not None:
-        self.settings.client_id = client_id
-    if client_secret is not None:
-        self.client_secret = client_secret
-
-    self.manage_user: bool = user_session is None
-    self.user_session: UserSession = UserSession(self.settings) if user_session is None else user_session
-      
-  # Implement destructor support
-  def __enter__(self):
-      if self.manage_user:
-          self.user_session.login()
-      return self
-
-  # Implement destructor support
-  def __exit__(self, exc_type, exc_value, traceback):
-      if self.manage_user:
-          self.logout()
-
-  def login(self) -> AccessToken:
-      return self.user_session.login()
-
-  def logout(self):
-      self.user_session.logout()
-        
+class LookerSDK(SDKBase):        
 `
   methodsEpilogue = ''
   modelsPrologue = `
