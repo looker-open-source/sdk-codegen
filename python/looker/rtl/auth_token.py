2640ecf399b2de11f1fb28204967b4223ab06295
jax <jax@looker.com>
2019-07-23 13:31:02 +0100

    style auth_token as per google's style guide.
    Also added Pipfile and Pipfile.lock with libraries required for code formatting and linting as --dev-dependencies

--- python/looker/rtl/auth_token.py
+++ python/looker/rtl/auth_token.py
@@ -1,19 +1,21 @@
-from looker.sdk.models import AccessToken
-import datetime
 from typing import Optional
+import datetime
+
+from looker.sdk import models as ml
 
 
-class AuthToken(object):
-    def __init__(self, token: Optional[AccessToken] = None):
+class AuthToken():
+    """Used to instantiate or check expiry of an AccessToken object"""
+    def __init__(self, token: Optional[ml.AccessToken] = None):
         self.access_token: str = ''
         self.token_type: str = ''
         self.expires_in: int = ''
         self.expires_at = datetime.datetime.now()
         if token is None:
-            token = AccessToken()
+            token = ml.AccessToken()
         self.set_token(token)
 
-    def set_token(self, token: AccessToken):
+    def set_token(self, token: ml.AccessToken):
         """Assign the token and set its expiration."""
         self.access_token = token.access_token or ''
         self.token_type = token.token_type or ''
@@ -27,7 +29,6 @@ class AuthToken(object):
             # set to expire 10 seconds ago
             exp = exp + datetime.timedelta(seconds=-10)
         self.expires_at = exp
-        return
 
     @property
     def is_active(self) -> bool:
