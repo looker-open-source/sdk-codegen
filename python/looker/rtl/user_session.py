15bad5442afa2a26414aa73de059f22ff08c0b4a
jax <jax@looker.com>
2019-07-31 15:36:40 +0100

    minor changes to UserSession
    - Got rid of the base class that was meant to act as an interface
    - Added docstrings to all public methods
    - Modified logout to make a request with a callable that provides the header required for the logout endpoint to succeed

--- python/looker/rtl/user_session.py
+++ python/looker/rtl/user_session.py
@@ -8,31 +8,7 @@ from looker.rtl import sdk_error as se
 from looker.rtl import api_settings as st
 
 
-class BaseUserSession(abc.ABC):
-    """UserSession base class"""
-    @property
-    @abc.abstractmethod
-    def is_impersonating(self) -> bool:
-        """Determines if user is impersonating another user"""
-    @property
-    @abc.abstractmethod
-    def is_authenticated(self) -> bool:
-        """Determines if current token is active"""
-    @abc.abstractmethod
-    def get_token(self) -> at.AuthToken:
-        """Returns an active token"""
-    @abc.abstractmethod
-    def login(self, user_id: Optional[str] = None) -> at.AuthToken:
-        """Logs in a user or allows an already logged in user to sudo as another user"""
-    @abc.abstractmethod
-    def authenticate(self) -> Dict[str, str]:
-        """Create a dictionary with the current token to be used as a header"""
-    @abc.abstractmethod
-    def logout(self) -> bool:
-        """Logs out current user"""
-
-
-class UserSession(BaseUserSession):
+class UserSession():
     def __init__(self,
                  settings: st.ApiSettings,
                  transport: Optional[tp.Transport] = None):
@@ -43,20 +19,24 @@ class UserSession(BaseUserSession):
 
     @property
     def is_authenticated(self) -> bool:
+        """Determines if current token is active."""
         if not (self._token and self._token.access_token):
             return False
         return self._token.is_active
 
     @property
     def is_impersonating(self) -> bool:
+        """Determines if user is impersonating another user."""
         return bool(self.user_id) and self.is_authenticated
 
-    def get_token(self):
+    def get_token(self) -> at.AuthToken:
+        """Returns an active token."""
         if not self.is_authenticated:
             self.login()
         return self._token
 
     def authenticate(self) -> Dict[str, str]:
+        """Create a dictionary with the current token to be used as a header."""
         token = self.get_token()
         header = {}
         if token and token.access_token:
@@ -64,6 +44,7 @@ class UserSession(BaseUserSession):
         return header
 
     def login(self, user_id: Optional[str] = None) -> at.AuthToken:
+        """Logs in a user or allows an already logged in user to sudo as another user."""
         if not self.is_authenticated:
             token = self._login(user_id)
             self._token = at.AuthToken(token)
@@ -91,19 +72,18 @@ class UserSession(BaseUserSession):
         return result
 
     def logout(self) -> bool:
-        result = False
+        """Logs out current active user."""
         if self.is_authenticated:
-            result = self._logout()
-        return bool(result)
+            self._logout()
+        return True
 
     def _logout(self) -> tp.TResponseValue:
         result = self._ok(
-            self.transport.request(tp.HttpMethod.DELETE,
-                                   '/logout',
-                                   authenticator={
-                                       'Authorization':
-                                       f'token {self._token.access_token}'
-                                   }))
+            self.transport.request(
+                tp.HttpMethod.DELETE,
+                '/logout',
+                authenticator=lambda:
+                {'Authorization': f'token {self._token.access_token}'}))
 
         # if no error was thrown, logout was successful
         if self.user_id:
