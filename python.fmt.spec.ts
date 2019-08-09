03848ac716c87698b348f2eb09ef2394ff94799c
Joel Dodge <joeldodge@gmail.com>
2019-08-08 17:41:11 -0700

    Python formatter updates
    - handle mapping reserved python words
    - cattr can't handle `ForwardRef()` types
    - all sdk model fields and method "in query" args are Optional[] = None

--- python.fmt.spec.ts
+++ python.fmt.spec.ts
@@ -41,12 +41,12 @@ describe('python formatter', () => {
             const param = apiModel.methods['run_query'].params[2]
             const actual =  fmt.declareParameter(indent, param)
             expect(actual).toEqual("# Row limit (may override the limit in the saved query).\n" +
-                "limit: int = 0")
+                "limit: Optional[int] = None")
         })
         it ('required typed parameter', () => {
-            const param = apiModel.methods['create_query'].params[0]
+            const param = apiModel.methods['create_query_render_task'].params[2]
             const actual =  fmt.declareParameter(indent, param)
-            expect(actual).toEqual(`# Requested fields.\nfields: str = ""`)
+            expect(actual).toEqual(`# Output width in pixels\nwidth: int`)
         })
     })
 
@@ -102,41 +102,60 @@ describe('python formatter', () => {
       it ('add_group_group', () => {
           const method = apiModel.methods['add_group_group']
           const args = fmt.httpArgs('', method).trim()
-          expect(args).toEqual("{'group_id': group_id}, None, body")
+          expect(args).toEqual("models.Group, body=body")
       })
       it ('create_query', () => {
-          // TODO get resolution working correctly
           const method = apiModel.methods['create_query']
           const args = fmt.httpArgs('', method).trim()
-          expect(args).toEqual("None, \n{'fields': fields}, body")
+          expect(args).toEqual('models.Query, query_params={"fields": fields}, body=body')
       })
       it ('create_dashboard', () => {
-        // TODO get resolution working correctly
         const method = apiModel.methods['create_dashboard']
         const args = fmt.httpArgs('', method).trim()
-        expect(args).toEqual("None, None, body")
+        expect(args).toEqual("models.Dashboard, body=body")
       })
     })
 
     describe('method signature', () => {
       it ('no params with all_datagroups', () => {
           const method = apiModel.methods['all_datagroups']
-          const expected = "# GET /datagroups -> List[Datagroup]\ndef all_datagroups(self) -> List[Datagroup]:\n"
+          const expected = "# GET /datagroups -> Sequence[models.Datagroup]\ndef all_datagroups(\n    self\n) -> Sequence[models.Datagroup]:\n"
           const actual = fmt.methodSignature('', method)
           expect(actual).toEqual(expected)
       })
-  })
+    })
+
+    describe('method body', () => {
+      it ('assert response is model add_group_group', () => {
+          const method = apiModel.methods['add_group_group']
+          const expected = 'response = self.post(f"/groups/{group_id}/groups", models.Group, body=body)\nassert isinstance(response, models.Group)\nreturn response'
+          const actual = fmt.httpCall(indent, method)
+          expect(actual).toEqual(expected)
+      })
+      it ('assert response is None delete_group_from_group', () => {
+          const method = apiModel.methods['delete_group_from_group']
+          const expected = 'response = self.delete(f"/groups/{group_id}/groups/{deleting_group_id}")\nassert response is None\nreturn response'
+          const actual = fmt.httpCall(indent, method)
+          expect(actual).toEqual(expected)
+      })
+      it ('assert response is list active_themes', () => {
+          const method = apiModel.methods['active_themes']
+          const expected = 'response = self.get(f"/themes/active", Sequence[models.Theme], query_params={"name": name, "ts": ts, "fields": fields})\nassert isinstance(response, list)\nreturn response'
+          const actual = fmt.httpCall(indent, method)
+          expect(actual).toEqual(expected)
+      })
+    })
 
     describe('type creation', () => {
         it ('with arrays and hashes', () => {
             const type = apiModel.types['Workspace']
             const actual =  fmt.declareType(indent, type)
-            expect(actual).toEqual("# Id of query\nquery_id: int")
+            expect(actual).toEqual('\n@attr.s(auto_attribs=True, kw_only=True)\nclass Workspace(model.Model):\n    """\n    Attributes:\n        id : The unique id of this user workspace. Predefined workspace ids include "production" and "dev"\n        projects : The local state of each project in the workspace\n        can : Operations the current user is able to perform on this object\n    """\n    # The unique id of this user workspace. Predefined workspace ids include "production" and "dev"\n    id: Optional[str] = None\n    # The local state of each project in the workspace\n    projects: Optional[Sequence["Project"]] = None\n    # Operations the current user is able to perform on this object\n    can: Optional[Sequence[bool]] = None')
         })
         it ('with refs, arrays and nullable', () => {
             const type = apiModel.types['ApiVersion']
             const actual =  fmt.declareType(indent, type)
-            expect(actual).toEqual("# Id of query\nquery_id: int")
+            expect(actual).toEqual('\n@attr.s(auto_attribs=True, kw_only=True)\nclass ApiVersion(model.Model):\n    """\n    Attributes:\n        looker_release_version : Current Looker release version number\n        current_version :\n        supported_versions : Array of versions supported by this Looker instance\n    """\n    # Current Looker release version number\n    looker_release_version: Optional[str] = None\n    current_version: Optional["ApiVersionElement"] = None\n    # Array of versions supported by this Looker instance\n    supported_versions: Optional[Sequence["ApiVersionElement"]] = None')
         })
     })
 })
--- python.fmt.ts
+++ python.fmt.ts
@@ -42,35 +42,107 @@ export class PythonFormatter extends CodeFormatter {
   indentStr = '    '
   endTypeStr = ''
 
+  // keyword.kwlist
+  pythonKeywords = [
+    'False',
+    'None',
+    'True',
+    'and',
+    'as',
+    'assert',
+    'async',
+    'await',
+    'break',
+    'class',
+    'continue',
+    'def',
+    'del',
+    'elif',
+    'else',
+    'except',
+    'finally',
+    'for',
+    'from',
+    'global',
+    'if',
+    'import',
+    'in',
+    'is',
+    'lambda',
+    'nonlocal',
+    'not',
+    'or',
+    'pass',
+    'raise',
+    'return',
+    'try',
+    'while',
+    'with',
+    'yield'
+  ]
+  pythonTypes: Record<string, IMappedType> = {
+    'number': { name: 'float', default: this.nullStr },
+    'double': { name: 'float', default: this.nullStr },
+    'integer': { name: 'int', default: this.nullStr },
+    'int32': { name: 'int', default: this.nullStr },
+    'int64': { name: 'int', default: this.nullStr },
+    'string': { name: 'str', default: this.nullStr },
+    'password': {name: 'str', default: this.nullStr },
+    'byte': {name: 'bytes', default: this.nullStr },
+    'boolean': {name: 'bool', default: this.nullStr },
+    'void': {name: 'None', default: this.nullStr },
+    'uri': {name: 'str', default: this.nullStr },
+    'datetime': {name: 'datetime.datetime', default: this.nullStr }
+  }
+
+  // Classes requiring custom structure/unstructure
+  reservedKeywordHooks: string[] = [
+    `\n\n# Custom converters for reserved keyword attribute classes`
+  ]
+  forwardRefHooks: string[] = [
+    `\n\n# Custom hooks for type induced forward references\n` +
+    `from typing import ForwardRef  # type: ignore  # noqa:E402\n`
+  ]
+
   // @ts-ignore
   methodsPrologue = (indent: string) => `
 # ${warnEditing}
-from typing import *
-# TODO why do we need "python.looker" for the imports?
-from python.looker.sdk.models import *
-from python.looker.rtl.sdk_base import SDKBase
+import datetime
+from typing import Optional, Sequence
 
-class LookerSDK(SDKBase):
+from looker.sdk import models
+from looker.rtl import api_methods
+
+
+class LookerSDK(api_methods.APIMethods):
 `
   // @ts-ignore
   methodsEpilogue = (indent: string) => ''
   // @ts-ignore
   modelsPrologue = (indent: string) => `
 # ${warnEditing}
+import datetime
+from typing import Optional, Sequence
+
 import attr
-from typing import *
+import cattr
+
+from looker.rtl import model
+from looker.rtl import serialize as sr
 `
   // @ts-ignore
-  modelsEpilogue = (indent: string) => ''
+  modelsEpilogue = (indent: string) => {
+    return this.forwardRefHooks.join('\n') + this.reservedKeywordHooks.join('\n')
+  }
 
   // @ts-ignore
   argGroup(indent: string, args: Arg[]) {
     if ((!args) || args.length === 0) return this.nullStr
     let hash: string[] = []
     for (let arg of args) {
-      hash.push(`'${arg}': ${arg}`)
+      hash.push(`"${arg}": ${arg}`)
     }
-    return `\n${indent}{${hash.join(this.argDelimiter)}}`
+    return `{${hash.join(this.argDelimiter)}}`
   }
 
   // @ts-ignore
@@ -86,29 +158,35 @@ from typing import *
   }
 
   declareProperty(indent: string, property: IProperty) {
-    const type = this.typeMap(property.type)
-    return this.commentHeader(indent, property.description)
-      + `${indent}${property.name}: ${type.name} = ${type.default}`
+    const propType = this.typeMapModels(property.type)
+    let propName = property.name
+    if (this.pythonKeywords.includes(propName)) {
+      propName = propName + '_'
+    }
+    const propTypeNameAndDefault = `Optional[${propType.name}] = ${propType.default}`
+    const propDef = `${indent}${propName}: ${propTypeNameAndDefault}`
+    return this.commentHeader(indent, property.description) + propDef
   }
 
   // because Python has named default parameters
   methodSignature(indent: string, method: IMethod) {
-    const type = this.typeMap(method.type)
+    const type = this.typeMapMethods(method.type)
     let bump = indent + this.indentStr
     let params: string[] = []
     const args = method.allParams
     if (args && args.length > 0) method.allParams.forEach(p => params.push(this.declareParameter(bump, p)))
     return this.commentHeader(indent, `${method.httpMethod} ${method.endpoint} -> ${type.name}`)
-      + `${indent}def ${method.name}(self${params.length > 0?",\n":''}${params.join(this.paramDelimiter)}) -> ${type.name}:\n`
+      + `${indent}def ${method.name}(\n${bump}self${params.length > 0?",\n":''}${params.join(this.paramDelimiter)}\n${indent}) -> ${type.name}:\n`
   }
 
   declareParameter(indent: string, param: IParameter) {
     let type = (param.location === strBody)
       ? this.writeableType(param.type) || param.type
       : param.type
-    const mapped = this.typeMap(type)
+    const mapped = this.typeMapMethods(type)
+    const paramType = (param.required ? mapped.name : `Optional[${mapped.name}]`)
     return this.commentHeader(indent, param.description)
-      + `${indent}${param.name}: ${mapped.name}`
+      + `${indent}${param.name}: ${paramType}`
       + (param.required ? '' : ` = ${mapped.default}`)
   }
 
@@ -141,9 +219,37 @@ from typing import *
   }
 
   httpArgs(indent: string, method: IMethod) {
-    let result = super.httpArgs(indent, method)
-    result = this.argFill(result, this.argGroup(indent, method.pathArgs))
-    return result
+      let result = this.argFill('', this.argGroup(indent, method.cookieArgs))
+      result = this.argFill(result, this.argGroup(indent, method.headerArgs))
+      if (method.bodyArg) {
+        result = this.argFill(result, `body=${method.bodyArg}`)
+      }
+      if (method.queryArgs.length) {
+        const queryParams = this.argGroup(indent, method.queryArgs)
+        result = this.argFill(result, `query_params=${queryParams}`)
+      }
+      const type = this.typeMapMethods(method.type)
+      result = this.argFill(result, type.name)
+      return result
+  }
+
+  httpCall(indent: string, method: IMethod) {
+      const bump = indent + this.indentStr
+      const args = this.httpArgs(bump, method)
+      const methodCall = `${indent}response = ${this.it(method.httpMethod.toLowerCase())}`
+      const callArgs = `f"${method.endpoint}"${args ? ", " +args: ""}`
+      let type = this.typeMapMethods(method.type).name
+      if (type.startsWith('Sequence')) {
+        type = 'list'
+      }
+      let assertion = `${indent}assert `
+      if (type == this.nullStr) {
+        assertion += `response is ${this.nullStr}`
+      } else {
+        assertion += `isinstance(response, ${type})`
+      }
+      const returnStmt = `${indent}return response`
+      return `${methodCall}(${callArgs})\n${assertion}\n${returnStmt}`
   }
 
   declareMethod(indent: string, method: IMethod) {
@@ -157,11 +263,31 @@ from typing import *
     const bump = this.bumper(indent)
     const b2 = this.bumper(bump)
     const attrs: string[] = []
-    Object.values(type.properties)
-        .forEach((prop) => attrs.push(`${b2}${prop.name} : ${prop.description}`))
+    let hasReservedWord = false
+    for (const prop of Object.values(type.properties)) {
+      let propName = prop.name
+      if (this.pythonKeywords.includes(propName)) {
+        propName = propName + '_'
+        hasReservedWord = true
+      }
+      let attr = `${b2}${propName} :`
+      if (prop.description) {
+        attr += ` ${prop.description}`
+      }
+      attrs.push(attr)
+    }
 
-    return `${indent}@attr.s(auto_attribs=True)\n` +
-        `${indent}class ${type.name}(object):\n` +
+    if (hasReservedWord) {
+      this.reservedKeywordHooks.push(
+        `cattr.register_structure_hook(${type.name}, sr.keyword_field_structure_hook)  # type: ignore`,
+        `cattr.register_unstructure_hook(${type.name}, sr.keyword_field_unstructure_hook)  # type: ignore\n`)
+    }
+    const refHandler = `lambda data, _: cattr.structure_attrs_fromdict(data, ${type.name})`
+    const register = `cattr.register_structure_hook(ForwardRef("${type.name}"), ${refHandler})  # type: ignore`
+    this.forwardRefHooks.push(register)
+    return `\n` +
+        `${indent}@attr.s(auto_attribs=True, kw_only=True)\n` +  // TODO: make "response" types frozen while "write" types are mutable
+        `${indent}class ${type.name}(model.Model):\n` +
         `${bump}"""\n` +
         (type.description ? `${bump}${type.description}\n\n` : '') +
         `${bump}Attributes:\n` +
@@ -173,32 +299,32 @@ from typing import *
     return text ? `${indent}"""${text}"""\n` : ''
   }
 
-  typeMap(type: IType): IMappedType {
-    // TODO why doesn't this work?
+  _typeMap(type: IType, format: 'models' | 'methods'): IMappedType {
     super.typeMap(type)
-    // type.refCount++
-    const pythonTypes: Record<string, IMappedType> = {
-      'number': { name: 'double', default: '0.0' },
-      'integer': { name: 'int', default: '0' },
-      'int32': { name: 'int', default: '0' },
-      'int64': { name: 'int', default: '0' },
-      'string': { name: 'str', default: '""' },
-      'password': {name: 'Password', default: this.nullStr},
-      'byte': {name: 'binary', default: this.nullStr},
-      'boolean': {name: 'bool', default: this.nullStr}
-    }
-
     if (type.elementType) {
-      const map  = this.typeMap(type.elementType)
-      // provisional typed list new in Python 3.5
-      // https://docs.python.org/3/package/typing.html
-      return {name: `List[${map.name}]`, default: 'attr.ib(factory=list)'}
+      const map  = this._typeMap(type.elementType, format)
+      return {name: `Sequence[${map.name}]`, default: this.nullStr}
     }
-
     if (type.name) {
-      return pythonTypes[type.name] || {name: type.name, default: this.nullStr }
+      let name: string
+      if (format == 'models') {
+        name = `"${type.name}"`
+      } else if (format == 'methods') {
+        name = `models.${type.name}`
+      } else {
+        throw new Error('format must be "models" or "methods"')
+      }
+      return this.pythonTypes[type.name] || {name: name, default: this.nullStr }
     } else {
       throw new Error('Cannot output a nameless type.')
     }
+  }
+
+  typeMapMethods(type: IType) {
+    return this._typeMap(type, 'methods')
+  }
+
+  typeMapModels(type: IType) {
+    return this._typeMap(type, 'models')
   }
 }
