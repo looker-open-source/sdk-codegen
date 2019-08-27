4ecfcb232515eb94ac23c77e1df3c4e8413f813b
John Kaster <johnk@looker.com>
2019-08-27 15:57:28 -0700

    WIP node packaging changes

---
+++ typescript/looker/index.ts
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Looker Data Sciences, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import { AuthSession, IAuthSession } from './rtl/authSession'
+import { LookerSDK } from './sdk/methods'
+import { ApiSettingsIniFile, IApiSettings } from './rtl/apiSettings'
+import { ITransport } from './rtl/transport'
+import { NodeTransport } from './rtl/nodeTransport'
+
+export * from './rtl/nodeTransport'
+// export * from './rtl/browserTransport' // TODO fix up the browserTransport implementation
+export * from './rtl/authToken'
+export * from './rtl/apiMethods'
+export * from './rtl/authSession'
+export * from './rtl/apiSettings'
+export * from './rtl/transport'
+export * from './sdk/methods'
+export * from './sdk/models'
+
+/**
+ * @class SDK
+ *
+ * Simple factory for the Looker SDK. Provides default connectivity for SDK methods
+ */
+export class SDK {
+  /**
+   * Creates an [[LookerSDK]] object.
+   *
+   * @param settings Defaults to the settings from LookerIni
+   *
+   * @param transport Defaults to a `NodeTransport` object
+   *
+   * @param session Defaults to `AuthSession` which logs in the user
+   */
+
+  static createClient(settings?: IApiSettings, transport?: ITransport, session?: IAuthSession) {
+    settings = settings || new ApiSettingsIniFile('looker.ini')
+    transport = transport || new NodeTransport(settings)
+    session = session || new AuthSession(settings, transport)
+    return new LookerSDK(session)
+  }
+
+}
