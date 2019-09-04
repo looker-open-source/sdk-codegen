0868fa60944015faed6d86762919744dba15cb38
jax <jax@looker.com>
2019-09-04 18:06:57 +0100

    test readibility improvements

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -2,7 +2,7 @@ import os.path
 import pytest  # type: ignore
 import sys
 import yaml
-from typing import cast, Dict, List
+from typing import cast, Dict, List, Union
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -12,58 +12,53 @@ from looker_sdk.sdk import methods as mtds  # noqa: E402
 
 
 @pytest.fixture(scope="session")  # type: ignore
-def looker_client():
-    return client.setup("../looker.ini")
-
-
-@pytest.fixture(scope="session", name="test_data")  # type: ignore
-def get_test_data():
-    with open("../test/data.yml") as f:
-        test_data = yaml.safe_load(f)
-
-    test_data["email_domain"] = "@testfoo.com"
-
-    return test_data
+def dashboards(_test_data):
+    return _test_data["dashboards"]
 
 
 @pytest.fixture(scope="session")  # type: ignore
-def dashboards(test_data):
-    return test_data["dashboards"]
+def queries(_test_data) -> List[Dict[str, str]]:
+    return _test_data["queries"]
 
 
 @pytest.fixture(scope="session")  # type: ignore
-def queries(test_data):
-    return test_data["queries"]
-
-
-@pytest.fixture(scope="session")  # type: ignore
-def users(test_data):
-    return test_data["users"]
+def users(_test_data) -> List[Dict[str, str]]:
+    return _test_data["users"]
 
 
 @pytest.fixture(scope="session")
-def email_domain(test_data):
-    return test_data["email_domain"]
+def email_domain(_test_data) -> str:
+    return _test_data["email_domain"]
+
+
+TTestData = Dict[str, Union[str, List[Dict[str, str]]]]
+
+
+@pytest.fixture(scope="session", name="_test_data")  # type: ignore
+def _get_test_data() -> TTestData:
+    with open("../test/data.yml") as f:
+        test_data = cast(TTestData, yaml.safe_load(f))
+    test_data["email_domain"] = "@testfoo.com"
+    return test_data
 
 
 @pytest.fixture(scope="function")  # type: ignore
 def remove_test_dashboards(looker_client: mtds.LookerSDK, dashboards):
-    # Clean up any test dashboards that may exist. We do this here instead at the end of
-    # tests in case we want to view the dashboards after the test
+    # Clean up any test dashboards that may exist. We do this at the beginning
+    # instead of the end of tests in case we want to view the dashboards after the test
     for d in dashboards:  # type: ignore
-        searched = looker_client.search_dashboards(
-            title=d.get("title", None)  # type: ignore
+        search_results = looker_client.search_dashboards(
+            title=d["title"]  # type: ignore
         )
-        if len(searched) > 0:
-            for dashboard in searched:
+        if len(search_results) > 0:
+            for dashboard in search_results:
                 looker_client.delete_dashboard(cast(str, dashboard.id))
 
 
-@pytest.fixture(name="test_users")  # type: ignore
-def create_test_users(looker_client: mtds.LookerSDK, test_data: Dict[str, str]):
-    # Create some users
-    users = cast(List[Dict[str, str]], test_data["users"])
-    email_domain = test_data["email_domain"]
+@pytest.fixture(name="test_users", scope="session")  # type: ignore
+def create_test_users(
+    looker_client: mtds.LookerSDK, users: List[Dict[str, str]], email_domain: str
+):
     user_ids: List[int] = []
 
     for u in users:
@@ -80,6 +75,10 @@ def create_test_users(looker_client: mtds.LookerSDK, test_data: Dict[str, str]):
 
     yield
 
-    # Clean up
     for user_id in user_ids:
         looker_client.delete_user(user_id)
+
+
+@pytest.fixture(scope="session")  # type: ignore
+def looker_client():
+    return client.setup("../looker.ini")
