99da58289264c5e3b2df0b70f7b3d930ebc10381
jax <jax@looker.com>
2019-09-11 17:23:31 +0100

    add section argument to client.setup

--- python/looker_sdk/client.py
+++ python/looker_sdk/client.py
@@ -1,5 +1,7 @@
 """Client entry point
 """
+from typing import Optional
+
 from looker_sdk.rtl import api_settings
 from looker_sdk.rtl import requests_transport
 from looker_sdk.rtl import serialize
@@ -7,10 +9,12 @@ from looker_sdk.rtl import auth_session
 from looker_sdk.sdk import methods
 
 
-def setup(config_file: str = "looker.ini") -> methods.LookerSDK:
+def setup(
+    config_file: str = "looker.ini", section: Optional[str] = None
+) -> methods.LookerSDK:
     """Default dependency configuration
     """
-    settings = api_settings.ApiSettings.configure(config_file)
+    settings = api_settings.ApiSettings.configure(config_file, section)
     settings.headers = {"Content-Type": "application/json"}
     transport = requests_transport.RequestsTransport.configure(settings)
     return methods.LookerSDK(
