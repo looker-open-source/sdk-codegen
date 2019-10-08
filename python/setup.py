da3a76b7512d187e0c9a44a4d5beb7ac3e1eb3c9
Joel Dodge <joel.dodge@looker.com>
2019-10-08 10:48:54 -0700

    Centralize sdk credentials storing advice (#64)
    Add section to base project README.md and link to it from each SDK readme

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "looker_sdk"
-VERSION = "0.1.3b3"
+VERSION = "0.1.3b4"
 REQUIRES = ["requests >= 2.22", "attrs", "cattrs >= 1.0.0rc0"]
 
 
@@ -16,6 +16,7 @@ setup(
     install_requires=REQUIRES,
     license="MIT",
     long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
     keywords=["looker_sdk", "Looker API 3.1"],
     name=NAME,
     package_data={"looker_sdk": ["py.typed", "looker_sdk/looker-sample.ini"]},
