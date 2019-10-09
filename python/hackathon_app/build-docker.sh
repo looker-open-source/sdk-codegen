4f1f2683e045f03ce61737a142a779f40cd7dcca
Joel Dodge <joeldodge@gmail.com>
2019-10-08 20:50:36 -0700

    Add frontend build to Dockerfile

--- python/hackathon_app/build-docker.sh
+++ python/hackathon_app/build-docker.sh
@@ -1,2 +1,5 @@
 set -e
+cd frontend
+yarn build
+cd ..
 docker build -t hackathon_app .
--- python/hackathon_app/server/main.py
+++ python/hackathon_app/server/main.py
@@ -1,3 +1,5 @@
+import os
+
 import flask
 from looker_sdk import client
 
@@ -14,3 +16,23 @@ def get_hackathons():
 @app.route("/register", methods=["POST"])
 def register():
     return flask.jsonify({"ok": True, "message": "Congratulations!"})
+
+
+@app.route("/")
+def main():
+    index_path = os.path.join(app.static_folder, "index.html")
+    return flask.send_file(index_path)
+
+
+# Everything not declared before (not a Flask route / API endpoint)...
+@app.route("/<path:path>")
+def route_frontend(path):
+    # ...could be a static file needed by the front end that
+    # doesn't use the `static` path (like in `<script src="bundle.js">`)
+    file_path = os.path.join(app.static_folder, path)
+    if os.path.isfile(file_path):
+        return flask.send_file(file_path)
+    # ...or should be handled by the SPA's "router" in front end
+    else:
+        index_path = os.path.join(app.static_folder, "index.html")
+        return flask.send_file(index_path)
