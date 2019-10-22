5e5fe74604189f47763b3f8f5bb8799e07551f71
Joel Dodge <joeldodge@gmail.com>
2019-10-22 06:38:15 -0700

    hackathon_app: first stab at adding user to looker instance

---
+++ python/hackathon_app/looker.py
@@ -0,0 +1,127 @@
+import functools
+
+from looker_sdk import client, methods, models, error
+
+
+def register_user(
+    *, hackathon: str, first_name: str, last_name: str, email: str, disable: bool = True
+) -> str:
+    sdk = client.setup()
+
+    user = find_or_create_user(
+        sdk=sdk, first_name=first_name, last_name=last_name, email=email
+    )
+    assert user.id
+    if not user.credentials_email:
+        create_email_credentials(sdk=sdk, user_id=user.id, email=email)
+    if not user.credentials_api3:
+        create_api3_credentials(sdk=sdk, user_id=user.id)
+    set_user_roles(sdk=sdk, user_id=user.id)
+    set_user_attributes(sdk=sdk, user_id=user.id, hackathon=hackathon)
+
+    ret = create_reset_password_link(sdk=sdk, user_id=user.id)
+
+    if disable:
+        disable_user(sdk=sdk, user_id=user.id)
+    return ret
+
+
+def find_or_create_user(
+    *, sdk: methods.LookerSDK, first_name: str, last_name: str, email: str
+) -> models.User:
+    try:
+        users = sdk.search_users(email=email)
+        if users:
+            user = users[0]
+            if (
+                user.first_name != first_name
+                or user.last_name != last_name
+                or user.is_disabled
+            ):
+                assert user.id
+                user = sdk.update_user(
+                    user_id=user.id,
+                    body=models.WriteUser(
+                        first_name=first_name, last_name=last_name, is_disabled=False
+                    ),
+                )
+        else:
+            user = sdk.create_user(
+                models.WriteUser(first_name=first_name, last_name=last_name)
+            )
+    except error.SDKError as create_ex:
+        raise RegisterError(f"Failed to find or create User ({create_ex})")
+    return user
+
+
+def try_to(func):
+    """Wrap API calls in try/except
+    """
+
+    @functools.wraps(func)
+    def wrapped_f(**kwargs):
+        try:
+            return func(**kwargs)
+        except error.SDKError as ex:
+            raise RegisterError(f"Failed to {func.__name__}: ({ex})")
+
+    return wrapped_f
+
+
+@try_to
+def create_email_credentials(*, sdk: methods.LookerSDK, user_id: int, email: str):
+    sdk.create_user_credentials_email(
+        user_id=user_id, body=models.WriteCredentialsEmail(email=email)
+    )
+
+
+@try_to
+def create_api3_credentials(*, sdk: methods.LookerSDK, user_id):
+    sdk.create_user_credentials_api3(user_id=user_id, body=models.CredentialsApi3())
+
+
+@try_to
+def set_user_roles(*, sdk: methods.LookerSDK, user_id):
+    hack_roles = {"Developer", "Hackathon"}
+    role_ids = []
+    for role in sdk.all_roles(fields="name,id"):
+        if role.name in hack_roles:
+            assert role.id
+            role_ids.append(role.id)
+    sdk.set_user_roles(user_id=user_id, body=role_ids)
+
+
+@try_to
+def set_user_attributes(*, sdk: methods.LookerSDK, user_id, hackathon):
+    user_attrs = sdk.all_user_attributes(fields="name,id")
+    for user_attr in user_attrs:
+        if user_attr.name == "hackathon":
+            hackathon_attr_id = user_attr.id
+            break
+    else:
+        raise RegisterError("Could not find 'hackathon' user attribute")
+    assert hackathon_attr_id
+    sdk.set_user_attribute_user_value(
+        user_id=user_id,
+        user_attribute_id=hackathon_attr_id,
+        body=models.WriteUserAttributeWithValue(value=hackathon),
+    )
+
+
+@try_to
+def create_reset_password_link(*, sdk: methods.LookerSDK, user_id: int) -> str:
+    ret = sdk.create_user_credentials_email_password_reset(
+        user_id=user_id, expires=False
+    ).password_reset_url
+    assert ret
+    return ret
+
+
+@try_to
+def disable_user(*, sdk: methods.LookerSDK, user_id: int):
+    sdk.update_user(user_id=user_id, body=models.WriteUser(is_disabled=True))
+
+
+class RegisterError(Exception):
+    """Failed to register user in looker instance.
+    """
---
+++ python/hackathon_app/tests/integration/test_looker.py
@@ -0,0 +1,55 @@
+from typing import List
+
+from looker_sdk import client, models
+
+import looker
+import sheets
+
+
+# TODO
+# - test errors
+# - remove user after test
+# - test with existing user
+
+
+def test_register_user(test_users: List[sheets.User]):
+
+    test_user = test_users[0]
+    test_hackathon = "Some Hackathon"
+
+    password_reset = looker.register_user(
+        hackathon=test_hackathon,
+        first_name=test_user.first_name,
+        last_name=test_user.last_name,
+        email=test_user.email,
+    )
+
+    assert password_reset
+
+    sdk = client.setup()
+    users = sdk.search_users(email=test_user.email)
+    assert len(users) > 0
+    actual_user = users[0]
+
+    assert actual_user.first_name == test_user.first_name
+    assert actual_user.last_name == test_user.last_name
+    assert actual_user.credentials_email
+    assert actual_user.credentials_api3
+    assert len(actual_user.credentials_api3) == 1
+    assert actual_user.role_ids
+    assert len(actual_user.role_ids) == 2
+
+    roles = sdk.all_roles(
+        fields="name", ids=models.DelimSequence(list(actual_user.role_ids))
+    )
+    assert {r.name for r in roles} == {"Developer", "Hackathon"}
+
+    assert actual_user.id
+    actual_attributes = sdk.user_attribute_user_values(user_id=actual_user.id)
+    assert actual_attributes
+    for actual_attribute in actual_attributes:
+        if actual_attribute.name == "hackathon":
+            assert actual_attribute.value == test_hackathon
+            break
+    else:
+        assert False, "Not assigned hackathon role"
