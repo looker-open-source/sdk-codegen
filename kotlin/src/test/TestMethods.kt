8a1049ce0c22700e17d396c982a82d51766090c5
John Kaster <johnk@looker.com>
2019-11-02 19:08:02 -0700

    WIP fleshing out SDK smoke tests a bit

--- kotlin/src/test/TestMethods.kt
+++ kotlin/src/test/TestMethods.kt
@@ -2,6 +2,7 @@ import com.looker.rtl.ApiSettingsIniFile
 import com.looker.rtl.SDKResponse
 import com.looker.rtl.Transport
 import com.looker.rtl.UserSession
+import com.looker.sdk.*
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.JacksonSerializer
@@ -11,14 +12,12 @@ import org.apache.http.conn.ssl.TrustSelfSignedStrategy
 import org.apache.http.ssl.SSLContextBuilder
 import kotlin.test.assertEquals
 import org.junit.Test as test
-import com.looker.sdk.LookerSDK
-import com.looker.sdk.WriteQuery
 import junit.framework.Assert.assertTrue
-import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class TestMethods {
-    val localIni = "/Users/Looker/Documents/sdk_codegen/looker.ini"
+//    val localIni = "/Users/Looker/Documents/sdk_codegen/looker.ini"
+    val localIni = "/Users/Looker/sdk-codegen/looker.ini"
     val settings = ApiSettingsIniFile(localIni, "Looker")
 
     val client: HttpClient = HttpClient(Apache) {
@@ -46,4 +45,44 @@ class TestMethods {
         assertTrue(response is SDKResponse.SDKSuccessResponse<*>)
         assertTrue((response as SDKResponse.SDKSuccessResponse<*>).ok)
     }
+
+    @test fun testAllUsers() {
+        // TODO Is there any way we can avoid having to typecast sdk.ok()?
+        // Problem here is there's no type verification between sdk.all_users() result and sdk.ok<foo>()
+        val list = sdk.ok<Array<User>>(sdk.all_users())
+        assertTrue(list.count() > 0, "Got users?")
+        for (item in list) {
+            // I see what you mean by the null check issue, Brian. We definitely want to solve this problem.
+            item.id?.let {id ->
+                val user = sdk.ok<User>(sdk.user(id))
+                val userId = user.id
+                assertEquals(userId, id)
+            }
+        }
+    }
+
+    @test fun testAllLooks() {
+        val list = sdk.ok<Array<Look>>(sdk.all_looks())
+        assertTrue(list.count() > 0, "Got looks?")
+        for (item in list) {
+            item.id?.let {id ->
+                val look = sdk.ok<Look>(sdk.look(id))
+                val gotId = look.id
+                assertEquals(gotId, id)
+            }
+        }
+    }
+
+    @test fun testAllDashboards() {
+        val list = sdk.ok<Array<DashboardBase>>(sdk.all_dashboards())
+        assertTrue(list.count() > 0, "Got dashboards?")
+        for (item in list) {
+            item.id?.let {id ->
+                val dashboard = sdk.ok<Dashboard>(sdk.dashboard(id))
+                val gotId = dashboard.id
+                assertEquals(gotId, id)
+            }
+        }
+    }
+
 }
