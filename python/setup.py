ce1d0c0db17d48d59eccaa870f2899d184cff69a
Joseph Axisa <jax@looker.com>
2019-11-13 11:29:18 +0000

    pin to python 3.7.x and add keywords (#92)
    * pin to python 3.7.x and add keywords
    
    * bump version to 0.1.3b5

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "looker_sdk"
-VERSION = "0.1.3b4"
+VERSION = "0.1.3b5"
 REQUIRES = ["requests >= 2.22", "attrs", "cattrs >= 1.0.0rc0"]
 
 
@@ -17,10 +17,11 @@ setup(
     license="MIT",
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
-    keywords=["looker_sdk", "Looker API 3.1"],
+    keywords=["Looker", "Looker API", "looker_sdk", "Looker API 3.1"],
     name=NAME,
     package_data={"looker_sdk": ["py.typed", "looker_sdk/looker-sample.ini"]},
     packages=find_packages(),
+    python_requires=">=3.7.0, <3.8",
     url="https://pypi.python.org/pypi/looker_sdk",
     version=VERSION,
 )
