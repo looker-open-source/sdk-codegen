5edbfc938340f35a3db28a76b69f745cdc89e22d
Joel Dodge <joeldodge@gmail.com>
2019-12-04 11:06:25 -0800

    Server google id_token verification tweaks

--- python/hackathon_app/server/main.py
+++ python/hackathon_app/server/main.py
@@ -4,8 +4,8 @@ from typing import Any
 
 import flask
 import flask_wtf  # type: ignore
-from google.oauth2 import id_token
-from google.auth.transport import requests
+from google.oauth2 import id_token  # type: ignore
+from google.auth.transport import requests  # type: ignore
 import wtforms  # type: ignore
 from wtforms import validators
 
@@ -62,11 +62,6 @@ def verify_google_token():
             "280777447286-iigstshu4o2tnkp5fjucrd3nvq03g5hs.apps.googleusercontent.com",
         )
 
-        # Or, if multiple clients access the backend server:
-        # idinfo = id_token.verify_oauth2_token(token, requests.Request())
-        # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-        #     raise ValueError('Could not verify audience.')
-
         if idinfo["iss"] not in ["accounts.google.com", "https://accounts.google.com"]:
             raise ValueError("Wrong issuer.")
 
@@ -75,9 +70,9 @@ def verify_google_token():
         #     raise ValueError('Wrong hosted domain.')
 
         # ID token is valid. Get the user's Google Account ID from the decoded token.
-    except ValueError:
+    except ValueError as ex:
         # Invalid token
-        return body
+        return {"error": str(ex)}
     return idinfo
 
 
