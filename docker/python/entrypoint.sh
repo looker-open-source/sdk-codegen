3b105712f785d49082e63da25648f7e7636e0709
Rodney Gomes <rodney.gomes@looker.com>
2019-12-04 18:54:19 -0800

    few tweaks you may like

--- docker/python/entrypoint.sh
+++ docker/python/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -exo pipefail
 
-cd /sdk-codegen
+cd /sdk_codegen
 yarn install
 
 # generate the various SDKs
 NODE_TLS_REJECT_UNAUTHORIZED='0' yarn sdk
 
-cd /sdk-codegen/python
+cd /sdk_codegen/python
 python setup.py install
 
 # now here run whatever tests!
+source /home/looker/.bash_profile
+python -m pytest --junitxml=sdk_codegen/results/test.xml /sdk_codegen/python/tests/integration/test_methods.py
