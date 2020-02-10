a40dec9ae1e880367b8d71377c622dc02d673f8e
John Kaster <johnk@looker.com>
2020-02-10 14:13:08 -0800

    Added tests that show how to override ApiSettings.readConfig() for Kotlin (#164)
    - also added language test switch options to `bin/smoke` along with a `-h` `--help` option

--- kotlin/src/test/TestApiSettings.kt
+++ kotlin/src/test/TestApiSettings.kt
@@ -1,6 +1,6 @@
 import com.looker.rtl.ApiSettings
-import com.looker.rtl.DEFAULT_API_VERSION
 import com.looker.rtl.DEFAULT_TIMEOUT
+import com.looker.rtl.UserSession
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -14,20 +14,58 @@ val quotedMinimum = """
 base_url='https://my.looker.com:19999'
 """.trimIndent()
 
+val mockId = "IdOverride"
+val mockSecret = "SecretOverride"
+
+class MockSettings(contents: String) : ApiSettings(contents) {
+    override fun readConfig(): Map<String, String> {
+        return mapOf(
+                "base_url" to baseUrl,
+                "verify_ssl" to verifySSL.toString(),
+                "timeout" to timeout.toString(),
+                "headers" to headers.toString(),
+                "client_id" to mockId,
+                "client_secret" to mockSecret
+        )
+    }
+}
+
 class TestApiSettings {
     @Test fun testApiSettingsDefaults() {
         val settings = ApiSettings(bareMinimum)
-        assertEquals(settings.apiVersion, DEFAULT_API_VERSION, "API version defaults to 3.1")
+        val config = settings.readConfig()
         assertEquals(settings.baseUrl, "https://my.looker.com:19999", "Base URL is read")
         assertEquals(settings.verifySSL, true)
         assertEquals(settings.timeout, DEFAULT_TIMEOUT)
+        assertEquals(config["client_id"], null)
+        assertEquals(config["client_secret"], null)
     }
 
     @Test fun testApiSettingsQuotes() {
         val settings = ApiSettings(quotedMinimum)
-        assertEquals(settings.apiVersion, DEFAULT_API_VERSION, "API version defaults to 3.1")
         assertEquals(settings.baseUrl, "https://my.looker.com:19999", "Base URL has no quotes")
         assertEquals(settings.verifySSL, true)
         assertEquals(settings.timeout, DEFAULT_TIMEOUT)
+    }
+
+    @Test fun testApiSettingsOverrides() {
+        val settings = MockSettings(bareMinimum)
+        val config = settings.readConfig()
+        assertEquals(settings.baseUrl, "https://my.looker.com:19999", "Base URL is read")
+        assertEquals(settings.verifySSL, true)
+        assertEquals(settings.timeout, DEFAULT_TIMEOUT)
+        assertEquals(config["client_id"], mockId)
+        assertEquals(config["client_secret"], mockSecret)
+    }
+
+    @Test fun testSessionOverride() {
+        val settings = MockSettings(bareMinimum)
+        val session = UserSession(settings)
+        val config = session.apiSettings.readConfig()
+        assertEquals(settings.baseUrl, "https://my.looker.com:19999", "Base URL is read")
+        assertEquals(settings.verifySSL, true)
+        assertEquals(settings.timeout, DEFAULT_TIMEOUT)
+        assertEquals(config["client_id"], mockId)
+        assertEquals(config["client_secret"], mockSecret)
     }
 }
