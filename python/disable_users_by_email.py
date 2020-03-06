a3112e00d6d339a77f647a3d8295bc8b035ff299
Joel Dodge <joeldodge@gmail.com>
2020-03-06 09:29:11 -0800

    Format python/disable_users_by_email.py with black

--- python/disable_users_by_email.py
+++ python/disable_users_by_email.py
@@ -1,14 +1,17 @@
-import csv
 import argparse
-from looker_sdk import client
+import csv
+
+import looker_sdk
 
 """
-The purpose of this script is to parse a CSV file containing a list of emails separated by line breaks. If
-the email in the file corresponds to an email in the Looker instance referred to in looker.ini, that user will
+The purpose of this script is to parse a CSV file containing a list of
+emails separated by line breaks. If the email in the file corresponds
+to an email in the Looker instance referred to in looker.ini, that user will
 be automatically disabled.
 """
 
-sdk = client.setup("looker.ini")
+
+sdk = looker_sdk.init31()
 
 
 def disable_user(user):
@@ -18,8 +21,14 @@ def disable_user(user):
 
 
 def parse_csv():
-    f = open(args.filename, 'r', encoding='utf-8-sig') # the encoding may need to be updated depending on your file, see https://stackoverflow.com/a/17912811
-    csv_reader = csv.reader(f, delimiter=',')
+    f = open(
+        args.filename,
+        "r",
+        # the encoding may need to be updated depending on your file,
+        # see https://stackoverflow.com/a/17912811
+        encoding="utf-8-sig",
+    )
+    csv_reader = csv.reader(f, delimiter=",")
 
     for line in csv_reader:
         for user in sdk.search_users(fields="id,email", email=line[0]):
@@ -28,6 +37,10 @@ def parse_csv():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filename", help="The path to the CSV file containing a list of emails to disable.", required=True)
+    parser.add_argument(
+        "--filename",
+        help="The path to the CSV file containing a list of emails to disable.",
+        required=True,
+    )
     args = parser.parse_args()
     parse_csv()
