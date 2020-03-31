c075f9369eeb8f98a6a36a8d1025cff3a6cef406
Rob Miller <rob@looker.com>
2020-03-31 14:34:18 -0700

    Add Codable to AuthToken.

--- swift/looker/rtl/authToken.swift
+++ swift/looker/rtl/authToken.swift
@@ -37,7 +37,7 @@ protocol AccessTokenProtocol {
 //  var expires_in: Int
 //}
 
-struct AuthToken: AccessTokenProtocol {
+struct AuthToken: AccessTokenProtocol, Codable {
     var access_token: String = ""
     var token_type: String = ""
     var expires_in: Int64 = 0
