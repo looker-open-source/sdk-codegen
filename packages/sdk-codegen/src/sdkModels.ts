7b5dc825c304c5c8b1c155dbd67e63c0bd54ac47
John Kaster <johnk@looker.com>
2020-04-24 14:44:36 -0700

    remove accidental node dependencies from sdkModels

--- packages/sdk-codegen/src/sdkModels.ts
+++ packages/sdk-codegen/src/sdkModels.ts
@@ -24,7 +24,7 @@
 
 import * as OAS from 'openapi3-ts'
 import md5 from 'blueimp-md5'
-import { HttpMethod, ResponseMode, responseMode, StatusCode } from '@looker/sdk'
+import { HttpMethod, ResponseMode, responseMode, StatusCode } from '@looker/sdk/dist/browser'
 import { IVersionInfo } from './codeGen'
 
 export const strBody = 'body'
