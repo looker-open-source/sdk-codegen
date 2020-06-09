d0dab431393a47c0a512631a090cf445769208dc
John Kaster <johnk@looker.com>
2020-06-09 15:12:12 -0700

    Updated language SDKs to Looker 7.10

--- python/looker_sdk/rtl/constants.py
+++ python/looker_sdk/rtl/constants.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-looker_version = "7.8"
+looker_version = "7.10"
 api_version = "4.0"
 sdk_version = f"{api_version}.{looker_version}"
 environment_prefix = "LOOKERSDK"
