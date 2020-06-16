6f541d32cc86ff0f0216a31dee6e479733e87bde
jax <jax@looker.com>
2020-06-16 18:45:06 +0100

    AuthToken and tests

---
+++ csharp/sdkrtl/AuthToken.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Security.Permissions;
+
+#nullable enable
+
+namespace Looker.RTL
+{
+    // TODO: replace IAccessToken with generated AccessToken interface
+    // TODO: figure out nullable ints
+    public interface IAccessToken
+    {
+        string access_token { get; set; }
+        string token_type { get; set; }
+        int expires_in { get; set; }
+        string refresh_token { get; set; }
+    }
+
+    /// <summary>
+    /// Concrete implementation of IAccessToken
+    /// </summary>
+    public struct AccessToken : IAccessToken
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+        public string refresh_token { get; set; }
+    }
+
+    /// <summary>
+    /// Used to instantiate or check expiry of an AccessToken struct
+    /// </summary>
+    public class AuthToken: IAccessToken
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+
+        public int expires_in { get; set; }
+
+        public string refresh_token { get; set; }
+        // Give a 10 second expiration window to account for latency
+        public DateTime ExpiresAt { get; set; } = DateTime.Now.AddSeconds(-10);
+
+        public AuthToken() { }
+
+        public AuthToken(IAccessToken token)
+        {
+            SetToken(token);
+        }
+
+        /// <summary>
+        /// Assign token and set its expiration
+        /// </summary>
+        /// <param name="token"></param>
+        public void SetToken(IAccessToken token)
+        {
+            if (!string.IsNullOrEmpty(token.token_type))
+            {
+                token_type = token.token_type;
+            }
+
+            if (!string.IsNullOrEmpty(token.refresh_token))
+            {
+                refresh_token = token.refresh_token;
+            }
+
+            expires_in = token.expires_in;
+            if (string.IsNullOrEmpty(token.access_token)) return;
+            access_token = token.access_token;
+            ExpiresAt = ExpiresAt.AddSeconds(expires_in);
+        }
+
+        /// <summary>
+        /// True if authentication token has not timed out
+        /// </summary>
+        /// <returns></returns>
+        public bool IsActive()
+        {
+            if (string.IsNullOrEmpty(access_token))
+            {
+                return false;
+            }
+            return ExpiresAt > DateTime.Now;
+        }
+    }
+}
