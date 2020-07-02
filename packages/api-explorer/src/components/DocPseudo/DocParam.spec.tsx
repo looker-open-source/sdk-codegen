fa7cd73f5dbc79d7cf7bf0c4683668b36b307b39
jax <jax@looker.com>
2020-07-02 11:31:08 +0100

    await all waitFor occurrences

--- packages/api-explorer/src/components/DocPseudo/DocParam.spec.tsx
+++ packages/api-explorer/src/components/DocPseudo/DocParam.spec.tsx
@@ -6,7 +6,7 @@ import { api } from '../../test-data'
 import { DocParam } from './DocParam'
 
 describe('DocParam', () => {
-  const allParams = api.methods.create_query.allParams
+  const allParams = api.methods.run_look.allParams
   const optionalParam = allParams.filter((val) => !val.required)[0]
   const requiredParam = allParams.filter((val) => val.required)[0]
 
@@ -15,12 +15,11 @@ describe('DocParam', () => {
     expect(screen.getByText(requiredParam.name)).toBeInTheDocument()
   })
 
-  test('it renders required params with a tooltip', () => {
+  test('it renders required params with a tooltip', async () => {
     renderWithTheme(<DocParam parameter={requiredParam} />)
     const arg = screen.getByText(requiredParam.name)
-
-    waitFor(() => {
-      fireEvent.mouseOver(arg)
+    fireEvent.mouseOver(arg)
+    await waitFor(() => {
       expect(screen.getByRole('tooltip')).toHaveTextContent(
         `${requiredParam.type.name} ${requiredParam.description}`
       )
