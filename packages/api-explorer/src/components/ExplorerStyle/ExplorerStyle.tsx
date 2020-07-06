da52d897d365f33856377103feffe9358189933c
jax <jax@looker.com>
2020-07-06 18:23:57 +0100

    Move all styling to ExplorerStyle

--- packages/api-explorer/src/components/ExplorerStyle/ExplorerStyle.tsx
+++ packages/api-explorer/src/components/ExplorerStyle/ExplorerStyle.tsx
@@ -1,14 +1,19 @@
 /*
+
  MIT License
+
  Copyright (c) 2020 Looker Data Sciences, Inc.
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -16,9 +21,145 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
+
  */
 
-import { css, createGlobalStyle } from 'styled-components'
+import styled, { css, createGlobalStyle } from 'styled-components'
+import {
+  Flex,
+  IconButton,
+  Sidebar,
+  SidebarGroup,
+  SidebarItem,
+} from '@looker/components'
+
+export const HEADER_HEIGHT = '66px'
+
+export const HeaderWrapper = styled(Flex).attrs({
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  height: HEADER_HEIGHT,
+  flexDirection: 'row',
+  pt: 'medium',
+  pr: 'large',
+  pb: 'medium',
+  pl: 'large',
+})`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.ui2};
+`
+
+interface SideNavWrapperProps {
+  headerHeight?: string
+}
+
+export const SideNavToggleWrapper = styled.div<SideNavWrapperProps>`
+  position: relative;
+  margin-top: ${parseInt(HEADER_HEIGHT, 10) / 2};
+
+  ${IconButton} {
+    background: #fff;
+    transform: translateX(-50%) translateY(-50%);
+    position: fixed;
+  }
+`
+
+interface SideNavStyleProps {
+  open: boolean
+}
+
+export const SideNavDivider = styled.div<SideNavStyleProps>`
+  transition: border 0.3s;
+  border-left: 1px solid
+    ${({ theme, open }) => (open ? theme.colors.ui2 : 'transparent')};
+  grid-area: divider;
+  overflow: visible;
+  position: relative;
+  &:hover {
+    border-color: ${({ theme, open }) =>
+      open ? theme.colors.ui3 : 'transparent'};
+  }
+`
+
+export const PageLayout = styled.div<SideNavStyleProps>`
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: ${({ open }) =>
+    open ? '20rem 0 50rem' : '1.5rem 0 50rem'};
+  grid-template-areas: 'sidebar divider main';
+  position: relative;
+
+  ${Sidebar} {
+    grid-area: sidebar;
+    width: 20rem;
+    z-index: 0;
+    padding: 0 ${({ theme }) => theme.space.medium};
+
+    button {
+      color: ${({ theme }) => theme.colors.text1};
+      cursor: pointer;
+      min-height: 2.25rem;
+
+      &:hover,
+      &:focus,
+      &[aria-expanded='true'] {
+        color: ${({ theme }) => theme.colors.key};
+      }
+    }
+  }
+
+  .codeMarker {
+    background: yellow;
+    position: absolute;
+  }
+
+  .hi {
+    background: yellow;
+  }
+
+  .main {
+    padding: ${({ theme }) => theme.space.xxlarge};
+  }
+
+  ${SidebarGroup} {
+    > div {
+      border-left: 1px dashed ${({ theme }) => theme.colors.ui2};
+      padding: ${({
+        theme: {
+          space: { xxsmall, xsmall },
+        },
+      }) => `${xxsmall} 0 ${xxsmall} ${xsmall}`};
+    }
+
+    .active > span {
+      background-color: ${({ theme }) => theme.colors.ui1};
+      font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+
+      div > div {
+        border: 1px solid ${({ theme }) => theme.colors.ui2};
+      }
+    }
+  }
+
+  ${SidebarItem} {
+    font-size: ${({ theme }) => theme.fontSizes.small};
+
+    & {
+      border-radius: 4px;
+      display: flex;
+      align-items: center;
+      padding: ${({ theme }) => theme.space.xsmall};
+    }
+
+    &:hover,
+    &:focus {
+      background-color: ${({ theme }) => theme.colors.ui1};
+    }
+  }
+
+  .doc {
+    grid-area: main;
+  }
+`
 
 const reset = css`
   html {
