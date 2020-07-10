215390c23ea8629c6ea9269900da4f924368474b
jax <jax@looker.com>
2020-07-10 18:21:41 +0100

    Included summary and endpoint in method search string

--- packages/sdk-codegen/src/sdkModels.ts
+++ packages/sdk-codegen/src/sdkModels.ts
@@ -1284,10 +1284,11 @@ export class Method extends SchemadSymbol implements IMethod {
     )
   }
 
-  searchString(criteria: SearchCriteria): string {
+  searchString(criteria: SearchCriteria = SearchAll): string {
     // Are we only searching for contained items of the method or not?
     if (!this.isMethodSearch(criteria)) return ''
     let result = super.searchString(criteria)
+    result += searchIt(this.summary) + searchIt(this.endpoint)
     if (criteria.has(SearchCriterion.method)) {
       if (this.rateLimited) {
         result += searchIt('rate limited')
