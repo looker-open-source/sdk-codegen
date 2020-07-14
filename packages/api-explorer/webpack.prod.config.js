1661864820a87499969b9f7177a3840947910d26
jax <jax@looker.com>
2020-07-14 16:21:58 +0100

    Added a webpack prod build config for Api Explorer

---
+++ packages/api-explorer/webpack.prod.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const PATHS = {
+  app: path.join(__dirname, 'src/index.tsx'),
+}
+
+module.exports = {
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: /src/,
+        sideEffects: false,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
