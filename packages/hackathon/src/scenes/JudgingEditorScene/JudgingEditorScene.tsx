01406c4ed5c16ff4a36bab1413b5a0416b79aa80
John Kaster <kaster@google.com>
2020-10-16 05:00:10 +0000

    WIP JudgingForm

--- packages/hackathon/src/scenes/JudgingEditorScene/JudgingEditorScene.tsx
+++ packages/hackathon/src/scenes/JudgingEditorScene/JudgingEditorScene.tsx
@@ -23,9 +23,147 @@
  SOFTWARE.
 
  */
-import React, { FC } from 'react'
+import React, { BaseSyntheticEvent, FC, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import {
+  Header,
+  Slider,
+  Button,
+  Heading,
+  ButtonOutline,
+  Space,
+  Text,
+} from '@looker/components'
+import { allJudgingsRequest } from '../../data/judgings/actions'
+import { getHackerState } from '../../data/hack_session/selectors'
+import { Judging, Judgings } from '../../models'
+import { Routes } from '../../routes/AppRouter'
 
-export const JudgingEditorScene: FC = () => {
-  // TODO: add a not found error in case project is not found
-  return <div>JudgingEditorScene</div>
+interface JudgingFormProps {
+  judgings: Judgings
+  judging: Judging
+}
+
+export const JudgingForm: FC<JudgingFormProps> = ({ judgings, judging }) => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(allJudgingsRequest())
+  }, [dispatch])
+  const history = useHistory()
+  const hacker = useSelector(getHackerState)
+  // const isLoading = useSelector(isLoadingState)
+  const [execution, setExecution] = useState<number>(0)
+  const [ambition, setAmbition] = useState<number>(0)
+  const [coolness, setCoolness] = useState<number>(0)
+  const [impact, setImpact] = useState<number>(0)
+
+  const onExecutionChange = (event: BaseSyntheticEvent) => {
+    setExecution(event.target.value)
+  }
+  const onAmbitionChange = (event: BaseSyntheticEvent) => {
+    setAmbition(event.target.value)
+  }
+
+  const onCoolnessChange = (event: BaseSyntheticEvent) => {
+    setCoolness(event.target.value)
+  }
+  const onImpactChange = (event: BaseSyntheticEvent) => {
+    setImpact(event.target.value)
+  }
+
+  const handleCancel = () => {
+    history.push(Routes.JUDGING)
+  }
+
+  setExecution(judging.execution)
+  setAmbition(judging.ambition)
+  setCoolness(judging.coolness)
+  setImpact(judging.impact)
+
+  const handleSave = () => {
+    judging.execution = execution
+    judging.ambition = ambition
+    judging.coolness = coolness
+    judging.impact = impact
+    if (judgings) {
+      console.log({ judging })
+      // await judgings.save(judging)
+    }
+  }
+
+  return (
+    <>
+      {judging && judging.user_id !== hacker.id && (
+        <Text>You can't judge this</Text>
+      )}
+      {judging && (
+        <>
+          <Header>{judging.$project.title}</Header>
+          <div>
+            <Slider
+              onChange={onExecutionChange}
+              value={execution}
+              min={1}
+              max={10}
+            />
+            <Heading>
+              <strong>Execution:</strong> {execution}
+            </Heading>
+          </div>
+          <div>
+            <Slider
+              onChange={onAmbitionChange}
+              value={ambition}
+              min={1}
+              max={10}
+            />
+            <Heading>
+              <strong>Ambition:</strong> {ambition}
+            </Heading>
+          </div>
+          <div>
+            <Slider
+              onChange={onCoolnessChange}
+              value={coolness}
+              min={1}
+              max={10}
+            />
+            <Heading>
+              <strong>Coolness:</strong> {coolness}
+            </Heading>
+          </div>
+          <div>
+            <Slider onChange={onImpactChange} value={impact} min={1} max={10} />
+            <Heading>
+              <strong>Impact:</strong> {impact}
+            </Heading>
+          </div>
+          <Space between width="100%">
+            <Space>
+              <ButtonOutline type="button" onClick={handleCancel}>
+                Cancel
+              </ButtonOutline>
+              <Button type="submit" onClick={handleSave}>
+                Save
+              </Button>
+            </Space>
+          </Space>
+        </>
+      )}
+    </>
+  )
+}
+
+interface JudgingEditorSceneProps {
+  judgings: Judgings
+  judging: Judging
+}
+
+export const JudgingEditorScene: FC<JudgingEditorSceneProps> = ({
+  judgings,
+  judging,
+}) => {
+  // TODO: add a not found error in case judging is not found
+  return <JudgingForm judgings={judgings} judging={judging} />
 }
