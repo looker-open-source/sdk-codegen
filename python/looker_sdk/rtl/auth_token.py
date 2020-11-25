0227b9a6f7f52963764d517e9fe70b2b3035e14c
Joseph Axisa <jax@looker.com>
2020-11-25 19:59:23 +0000

    Refactored AuthToken to be independent of generated models

--- python/looker_sdk/rtl/auth_token.py
+++ python/looker_sdk/rtl/auth_token.py
@@ -22,41 +22,68 @@
 
 """AuthToken
 """
-from typing import Optional, Type, Union
+from typing import Optional, Type, Union, cast
 import datetime
 
-from looker_sdk.rtl import constants
-from looker_sdk.sdk.api31 import models as models31
-from looker_sdk.sdk.api40 import models as models40
+import attr
+
+from looker_sdk.rtl import auth_session
+from looker_sdk.rtl import model
 
 
-token_model: Union[Type[models31.AccessToken], Type[models40.AccessToken]]
-if constants.api_version == "3.1":
-    token_model = models31.AccessToken
-elif constants.api_version == "4.0":
-    token_model = models40.AccessToken
+#  Same as the Looker API access token object
+#  Re-declared here to be independent of model generation
+@attr.s(auto_attribs=True, init=False)
+class AccessToken(model.Model):
+    """
+    Attributes:
+        access_token: Access Token used for API calls
+        token_type: Type of Token
+        expires_in: Number of seconds before the token expires
+        refresh_token: Refresh token which can be used to obtain a new access token
+    """
+
+    access_token: Optional[str] = None
+    token_type: Optional[str] = None
+    expires_in: Optional[int] = None
+    refresh_token: Optional[str] = None
+
+    def __init__(
+        self,
+        *,
+        access_token: Optional[str] = None,
+        token_type: Optional[str] = None,
+        expires_in: Optional[int] = None,
+        refresh_token: Optional[str] = None,
+    ):
+        self.access_token = access_token
+        self.token_type = token_type
+        self.expires_in = expires_in
+        self.refresh_token = refresh_token
 
 
 class AuthToken:
     """Used to instantiate or check expiry of an AccessToken object"""
 
     def __init__(
-        self, token: Optional[Union[models31.AccessToken, models40.AccessToken]] = None
+        self, token: Optional[AccessToken] = None,
     ):
         self.lag_time = 10
         self.access_token: str = ""
         self.refresh_token: str = ""
         self.token_type: str = ""
         self.expires_in: int = 0
-        self.expires_at = datetime.datetime.now() + datetime.timedelta(seconds=-self.lag_time)
+        self.expires_at = datetime.datetime.now() + datetime.timedelta(
+            seconds=-self.lag_time
+        )
         if token is None:
-            token = token_model()
-        self.set_token(token)
+            token = AccessToken()
+        self.set_token(cast(AccessToken, token))
 
-    def set_token(self, token: Union[models31.AccessToken, models40.AccessToken]):
+    def set_token(self, token: AccessToken):
         """Assign the token and set its expiration."""
         self.access_token = token.access_token or ""
-        if isinstance(token, models40.AccessToken):
+        if isinstance(token, AccessToken):
             self.refresh_token = token.refresh_token or ""
         self.token_type = token.token_type or ""
         self.expires_in = token.expires_in or 0
