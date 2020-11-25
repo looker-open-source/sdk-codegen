ae7c2c73d06211fe596f48a1230032ae636faaec
John Kaster <kaster@google.com>
2020-11-25 15:03:01 -0800

    Smoke and unit tests for Swift

---
+++ swift/looker/Tests/lookerTests/smokeTests.swift
@@ -0,0 +1,32 @@
+//
+//  smokeTests.swift
+//  lookerTests
+//
+//  Created by John Kaster on 11/25/20.
+//
+
+import XCTest
+
+class smokeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
