c42ceab0c22bae67ac025973f8d632056780f13f
Joel Dodge <joeldodge@google.com>
2020-12-08 15:24:28 +0000

    say what?

--- python/tests/rtl/test_serialize.py
+++ python/tests/rtl/test_serialize.py
@@ -623,7 +623,7 @@ def test_serialize_single():
         class_="model-name",
         finally_=[1, 2, 3],
     )
-    expected = json.dumps(MODEL_DATA).encode("utf-8") + "asdf"
+    expected = json.dumps(MODEL_DATA).encode("utf-8") + "asdfff"
     assert sr.serialize(model) == expected
 
 
