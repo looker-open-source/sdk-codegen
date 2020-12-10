5681b973dcc20bf60f0e2adb92d29853ccc50b5c
Joel Dodge <joeldodge@google.com>
2020-12-10 02:04:48 +0000

    ci(python): setting up release-please for python

--- python/looker_sdk/version.py
+++ python/looker_sdk/version.py
@@ -20,42 +20,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-"""The setup script."""
-
-from setuptools import setup, find_packages  # noqa: H301
-
-version = {}
-with open("looker_sdk/version.py") as fp:
-    exec(fp.read(), version)
-
-NAME = "lktstpy"
-VERSION = version["__version__"]
-REQUIRES = [
-    "requests >= 2.22",
-    # Python 3.6
-    "attrs >= 18.2.0;python_version<'3.7'",
-    "cattrs < 1.1.0;python_version<'3.7'",
-    "python-dateutil;python_version<'3.7'",
-    # Python 3.7+
-    "attrs >= 20.1.0;python_version>='3.7'",
-    "cattrs >= 1.1.0;python_version>='3.7'",
-    "typing-extensions;python_version<'3.8'",
-]
-
-
-setup(
-    author="Looker Data Sciences, Inc.",
-    author_email="support@looker.com",
-    description="Looker API 3.1",
-    install_requires=REQUIRES,
-    license="MIT",
-    long_description=open("README.rst").read(),
-    long_description_content_type="text/x-rst",
-    keywords=["Looker", "Looker API", "looker_sdk", "Looker API 3.1"],
-    name=NAME,
-    package_data={"looker_sdk": ["py.typed", "looker_sdk/looker-sample.ini"]},
-    packages=find_packages(),
-    python_requires="~=3.6",
-    url="https://pypi.python.org/pypi/looker_sdk",
-    version=VERSION,
-)
+__version__ = "7.20.0"
