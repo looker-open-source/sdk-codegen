874376afbaf52f30634a445f605f6a8b2673db09
John Kaster <kaster@google.com>
2021-01-08 13:13:12 -0800

    Update packages/api-explorer/src/components/ExploreType/ExploreType.spec.tsx
    Co-authored-by: Joseph Axisa <jax@looker.com>

--- packages/api-explorer/src/components/ExploreType/ExploreType.spec.tsx
+++ packages/api-explorer/src/components/ExploreType/ExploreType.spec.tsx
@@ -111,6 +111,7 @@ describe('ExploreType', () => {
           <ExploreTypeLink type={targetType} />
         </MemoryRouter>
       )
+
       const actual = screen.getByText(targetType.jsonName)
       expect(actual).toBeInTheDocument()
       expect(actual).toHaveProperty(
