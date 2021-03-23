dfa291fd41746ae758e0becd802ca3a7e8bf8b92
Izzy Miller <izzy@looker.com>
2021-03-22 21:56:13 -0700

    format

--- examples/python/lookersdk-flask/app/__init__.py
+++ examples/python/lookersdk-flask/app/__init__.py
@@ -6,16 +6,17 @@ from . import auth
 from . import blog
 from . import looker
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'lookersdk-flask.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "lookersdk-flask.sqlite"),
     )
 
     if test_config is None:
-        #Load real config unless we are testing
-        app.config.from_pyfile('config.py', silent=True)
+        # Load real config unless we are testing
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
     try:
@@ -23,10 +24,10 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    #Initialize app and register our blueprints
+    # Initialize app and register our blueprints
     db.init_app(app)
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+    app.add_url_rule("/", endpoint="index")
 
     return app
--- examples/python/lookersdk-flask/app/auth.py
+++ examples/python/lookersdk-flask/app/auth.py
@@ -2,82 +2,95 @@ import functools
 import os
 
 from flask import (
-    Blueprint, flash, redirect, render_template, request, session, url_for, current_app
+    Blueprint,
+    flash,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+    current_app,
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from .db import get_db
 from .looker import get_my_user
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         db = get_db()
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = 'User {} is already registered.'.format(username)
+            error = "Password is required."
+        elif (
+            db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone()
+            is not None
+        ):
+            error = "User {} is already registered.".format(username)
 
         if error is None:
             db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
+                "INSERT INTO user (username, password) VALUES (?, ?)",
+                (username, generate_password_hash(password)),
             )
             db.commit()
             flash("Successfully registered. You may now log in.")
-            return redirect(url_for('auth.login'))
+            return redirect(url_for("auth.login"))
         flash(error)
-    return render_template('auth/register.html')
+    return render_template("auth/register.html")
 
-@bp.route('/login', methods=('GET', 'POST'))
+
+@bp.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            "SELECT * FROM user WHERE username = ?", (username,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
 
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
-            session['user_name'] = user['username']
-            return redirect(url_for('index'))
+            session["user_id"] = user["id"]
+            session["user_name"] = user["username"]
+            return redirect(url_for("index"))
         flash(error)
-    return render_template('auth/login.html')
+    return render_template("auth/login.html")
 
-#This fires before every request and loads in the looker base URL to be available in the header
+
+# This fires before every request and loads in the looker base URL to be available in the header
 @bp.before_app_request
 def load_instance():
-    session['lookerurl'] = os.environ.get('LOOKERSDK_BASE_URL')
+    session["lookerurl"] = os.environ.get("LOOKERSDK_BASE_URL")
 
-@bp.route('/logout')
+
+@bp.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
+
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        if session['user_id'] is None:
-            return redirect(url_for('auth.login'))
+        if session["user_id"] is None:
+            return redirect(url_for("auth.login"))
         return view(**kwargs)
+
     return wrapped_view
