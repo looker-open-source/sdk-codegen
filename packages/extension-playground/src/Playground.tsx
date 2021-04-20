43ca1b35eda42fd3f39aaa9b62715d4913664213
Bryn Ryans <bryn.ryans@looker.com>
2021-04-19 17:36:35 -0700

    Remove commented out code

--- packages/extension-playground/src/Playground.tsx
+++ packages/extension-playground/src/Playground.tsx
@@ -26,29 +26,18 @@
 import React from 'react'
 import { ComponentsProvider, Space, Text } from '@looker/components'
 
-// import { Button, ComponentsProvider, Space, Text } from '@looker/components'
-// import { ExtensionContext } from '@looker/extension-sdk-react'
-
 /**
  * Playground for testing extension SDK functionality.
  * Changes are not expected to be kept and may be thrown
  * away at anytime. Keep this simple.
  */
 export const Playground: React.FC = () => {
-  // const { extensionSDK } = useContext(ExtensionContext)
-
-  // const onClipboardClick = () =>
-  //   extensionSDK.clipboardWrite(
-  //     `伝開ユオヤ前刻むけト休表日ウシスト掲類カイモヨ任3書トヱム味社ム一首分のル社変へらや稿働クノヒタ振会今ー法科太せぜすづ。差キケリカ沈在ヤリ覧打ぱばい次宅ヒレ重暮サ敬思カナ受関ぴ全遠ぶト考著らよ岡火ネ田技龍政クレぴざ着投べほ照54中消でじきょ朝苦なふのぼ。能ぜむさ及人がずみし提村よならに読公分ド体41本モキネ高浦国むらかフ動規ず藤井ノキサ部形浩ッぎな。`
-  //   )
-
   return (
     <ComponentsProvider>
       <Space p="xxxxxlarge" width="100%" height="50vh" around>
         <Text p="xxxxxlarge" fontSize="xxxxxlarge">
           Welcome to the Playground
         </Text>
-        {/* <Button onClick={onClipboardClick}>Write text to clipboard</Button> */}
       </Space>
     </ComponentsProvider>
   )
