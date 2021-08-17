a10245aa7ea180670d9d8219810aab88eb50854d
John Kaster <kaster@google.com>
2021-08-17 08:51:05 -0700

    fix: spec refresh (#801)
    We changes spec fetching to async which made `yarn refresh` fail

--- packages/sdk-codegen-scripts/scripts/utils.ts
+++ packages/sdk-codegen-scripts/scripts/utils.ts
@@ -42,17 +42,6 @@ const homeToRoost = '../../../'
 const getRootPath = () => path.join(__dirname, homeToRoost)
 const rootFile = (fileName = '') => path.join(getRootPath(), fileName)
 
-export const apixSpecFileName = (fileName: string) => {
-  const p = path.parse(fileName)
-  return rootFile(`spec/${p.base}`)
-}
-
-// const copySpec = (fileName: string) => {
-//   const dest = apixSpecFileName(fileName)
-//   fs.copyFileSync(fileName, dest)
-//   console.info(`Copied ${fileName} to ${dest}`)
-// }
-
 export const updateSpecs = async (apiVersions = supportedApiVersions) => {
   const iniFile = rootFile('looker.ini')
   const config = SDKConfig(iniFile)
@@ -63,7 +52,7 @@ export const updateSpecs = async (apiVersions = supportedApiVersions) => {
     } in ${iniFile} ...`
   )
   const lookerVersions = await fetchLookerVersions(props)
-  const specs = getSpecsFromVersions(lookerVersions)
+  const specs = await getSpecsFromVersions(lookerVersions)
   for (const v of apiVersions) {
     try {
       const specFile = await logConvertSpec(name, specs[v], lookerVersions)
