e2a20907bf71379da525025051ca91f74ea40117
Luka Fontanilla <maluka@google.com>
2021-08-18 17:08:21 -0700

    Revert "updated schedule files, removed allUsers parameter and updated parameter passing with valid typescript/javascript"
    This reverts commit b20b9ddfe6d6ccc3f25b89df9ca2b72d523c5b41.

--- examples/typescript/bulkDisableSchedules.ts
+++ examples/typescript/bulkDisableSchedules.ts
@@ -5,7 +5,7 @@ const sdk = LookerNodeSDK.init40()
 
 // get schedules for all users (will require admin permissions for api caller) or for a specific user by id
 
-const getSchedules = async (userID?: number): Promise<IScheduledPlan[]> => {
+const getSchedules = async (allUsers: boolean, userID?: number): Promise<IScheduledPlan[]> => {
     let schedules: IScheduledPlan[];
     if(userID) {
         schedules = await sdk.ok(sdk.all_scheduled_plans({user_id: userID, all_users:false}))
@@ -15,22 +15,22 @@ const getSchedules = async (userID?: number): Promise<IScheduledPlan[]> => {
     return schedules
 }
 
-const bulkDisableSchedules = async (userID?: number): Promise<boolean | Error> => {
-    const schedules = await getSchedules(userID)
-    try {
-        schedules.forEach(async (s) => {
-            if (s.id) {
-                await sdk.ok(sdk.update_scheduled_plan(s.id, {enabled:false}))
-            }
-        })
-        // depending on whether userID is included or not we will change what we log to the console
-        console.log(`Successfully disabled all schedules ${userID ? `for user ${userID}.` : 'for all users.'}`)
-        return true
-    } catch (e) {
-        throw new Error(`There was an error trying to disable schedules. Full error message: ${e}`)
+const bulkDisableSchedules = async (allUsers: boolean, userID?: number): Promise<boolean> => {
+    if(userID || allUsers) {
+        const schedules = await getSchedules(allUsers=allUsers, userID=userID)
+        try {
+            schedules.forEach(async (s) => await sdk.ok(sdk.update_scheduled_plan(s.id, {enabled:false})))
+            // depending on whether allUsers is set to true or not we will change what we log to the console
+            console.log(`Successfully disabled all schedules ${allUsers ? 'for all users' : `for user ${userID}`}`)
+            return true
+        } catch (e) {
+            throw new Error(`There was an error trying to disable schedules. Full error message: ${e}`)
+        }
+    } else {
+        throw new Error('Please specify either allUsers or a userID to fetch schedules.')
     }
 }
 
-// Examples
-// bulkDisableSchedules()
-// bulkDisableSchedules(938)
+// Example
+// bulkDisableSchedules(false, 300)
+// bulkDisableSchedules(true)
--- examples/typescript/bulkReassignSchedules.ts
+++ examples/typescript/bulkReassignSchedules.ts
@@ -1,11 +1,10 @@
 import {LookerNodeSDK} from '@looker/sdk-node'
 import {IScheduledPlan} from '@looker/sdk'
-import { user } from '@looker/sdk/lib/3.1/funcs';
 
 const sdk = LookerNodeSDK.init40()
 
 // schedules will either return for all users or for a specific user id if passed
-const getSchedules = async (userID?: number): Promise<IScheduledPlan[]> => {
+const getSchedules = async (allUsers: boolean, userID?: number): Promise<IScheduledPlan[]> => {
     let schedules: IScheduledPlan[];
     if(userID) {
         schedules = await sdk.ok(sdk.all_scheduled_plans({user_id: userID, all_users:false}))
@@ -15,26 +14,22 @@ const getSchedules = async (userID?: number): Promise<IScheduledPlan[]> => {
     return schedules
 }
 
-const bulkReAssignSchedules = async (newOwner: number, userID?: number): Promise<boolean | Error> => {
-    if(newOwner) {
-        const schedules = await getSchedules(userID)
+const bulkReAssignSchedules = async (allUsers: boolean, newOwner: number, userID?: number): Promise<boolean | Error> => {
+    if((userID && newOwner) || (allUsers && newOwner)) {
+        const schedules = await getSchedules(allUsers=allUsers, userID=userID)
         try {
-            schedules.forEach(async (s) => {
-                if (s.id) {
-                    await sdk.ok(sdk.update_scheduled_plan(s.id, {user_id:newOwner}))
-                }
-            })
-            // depending on whether userID is included or not we will change what we log to the console
-            console.log(`Successfully reassigned all schedules ${userID ? `for user ${userID}` : 'for all users'} to ${newOwner}`)
+            schedules.forEach(async (s) => await sdk.ok(sdk.update_scheduled_plan(s.id, {user_id:newOwner})))
+            // depending on whether allUsers is set to true or not we will change what we log to the console
+            console.log(`Successfully reassinged all schedules ${allUsers? 'for all users' : `for user ${userID}`} to ${newOwner}`)
             return true
         } catch (e) {
             throw new Error(`There was an error trying to disable schedules. Full error message: ${e}`)
         }
     } else {
-        throw new Error('Please specify at least the id of the new owner for schedules.')
+        throw new Error('Please specify either allUsers and a newOwner or a userID and a newOwner to fetch schedules.')
     }
 }
 
 // Examples
-// bulkDisableSchedules(200)
-// bulkDisableSchedules(200, 300)
+// bulkDisableSchedules(false, 200, 300)
+// bulkDisableSchedules(true, 200)
