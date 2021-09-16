29b030285c0f214a8c14ee320a47f599fae91d01
Luka Fontanilla <maluka@google.com>
2021-09-16 13:25:36 -0700

    Formatting with prettier checks

--- examples/typescript/bulkDisableSchedules.ts
+++ examples/typescript/bulkDisableSchedules.ts
@@ -1,34 +1,44 @@
-import {LookerNodeSDK} from '@looker/sdk-node'
-import {IScheduledPlan} from '@looker/sdk'
+import { LookerNodeSDK } from '@looker/sdk-node'
+import { IScheduledPlan } from '@looker/sdk'
 
 const sdk = LookerNodeSDK.init40()
 
 // get schedules for all users (will require admin permissions for api caller) or for a specific user by id
 
 const getSchedules = async (userID?: number): Promise<IScheduledPlan[]> => {
-    let schedules: IScheduledPlan[];
-    if(userID) {
-        schedules = await sdk.ok(sdk.all_scheduled_plans({user_id: userID, all_users:false}))
-    } else {
-        schedules = await sdk.ok(sdk.all_scheduled_plans({all_users:true}))
-    }
-    return schedules
+  let schedules: IScheduledPlan[]
+  if (userID) {
+    schedules = await sdk.ok(
+      sdk.all_scheduled_plans({ user_id: userID, all_users: false })
+    )
+  } else {
+    schedules = await sdk.ok(sdk.all_scheduled_plans({ all_users: true }))
+  }
+  return schedules
 }
 
-const bulkDisableSchedules = async (userID?: number): Promise<boolean | Error> => {
-    const schedules = await getSchedules(userID)
-    try {
-        schedules.forEach(async (s) => {
-            if (s.id) {
-                await sdk.ok(sdk.update_scheduled_plan(s.id, {enabled:false}))
-            }
-        })
-        // depending on whether userID is included or not we will change what we log to the console
-        console.log(`Successfully disabled all schedules ${userID ? `for user ${userID}.` : 'for all users.'}`)
-        return true
-    } catch (e) {
-        throw new Error(`There was an error trying to disable schedules. Full error message: ${e}`)
-    }
+const bulkDisableSchedules = async (
+  userID?: number
+): Promise<boolean | Error> => {
+  const schedules = await getSchedules(userID)
+  try {
+    schedules.forEach(async (s) => {
+      if (s.id) {
+        await sdk.ok(sdk.update_scheduled_plan(s.id, { enabled: false }))
+      }
+    })
+    // depending on whether userID is included or not we will change what we log to the console
+    console.log(
+      `Successfully disabled all schedules ${
+        userID ? `for user ${userID}.` : 'for all users.'
+      }`
+    )
+    return true
+  } catch (e) {
+    throw new Error(
+      `There was an error trying to disable schedules. Full error message: ${e}`
+    )
+  }
 }
 
 // Examples
