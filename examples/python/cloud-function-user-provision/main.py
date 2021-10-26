a3e8ed2cccb0ab4d03b7fcfcc792c6b0016c4665
Lan Tran <lantrann@google.com>
2021-10-26 06:56:41 +0000

    add in examples for gsheet integration

--- examples/python/cloud-function-user-provision/main.py
+++ examples/python/cloud-function-user-provision/main.py
@@ -1,16 +1,28 @@
-"""This Cloud Function leverages Looker Python SDK to manage user provision. The 
-`main` function is used as the entry point to the code. It takes an email address 
-as an input through a POST request, then checks if this email has been associated
-with an existing Looker user. If an exisiting user is found, then an email to 
-reset password will be sent. Otherwise, a new user will be created, and a setup email
-will be sent.
+"""This Cloud Function leverages Looker Python SDK to manage user provision. 
+It takes an email address as an input, then checks if this email has been 
+associated with an existing Looker user. If a current user is found, then an 
+email to reset the password will be sent. Otherwise, a new user will be created, 
+and a setup email will be sent.
+
+The `main` function is triggered through an HTTP request. Two example approaches 
+are provided below:
+  main(request): take a POST request in form of {"email":"test@test.com"}, 
+    and read the email value from the request body
+  main_gsheet(request): take a GET request and read the email value from a cell
+    inside an existing Google sheet. 
 
 HTTP Cloud Functions: https://cloud.google.com/functions/docs/writing/http#sample_usage"""
+
+# If not using Google Sheet, removing `gspread` here and in `requirements.txt`
+import gspread
 
 import looker_sdk
 sdk = looker_sdk.init40()
 
+# [START main(request)]
 def main(request):
+  """Take email from JSON body of a POST request, and use the email value 
+  as an input for looker_user_provision() function"""
   try: 
     request_json = request.get_json()
     email = request_json["email"]
@@ -18,7 +30,39 @@ def main(request):
     return result 
   except:
     return 'Please provide JSON in the format of {"email":"test@test.com"}'
+# [END main(request)]
+
+# [START main_gsheet(request)]
+def main_gsheet(request):
+  """Take email from a cell inside an existing Google Sheet, leveraging 
+  GCP's service accounts and gspread, Google Sheet's Python module"""
+  try: 
+    email = get_email_from_sheet()
+    result = looker_user_provision(email=email)
+    return result 
+  except:
+    return 'An error occurred.'
+
+def get_email_from_sheet():
+  """Authenticate to an existing Google Sheet using a service account. 
+  GCP's Cloud Function is often run unattended, so a service account 
+  (a machine-to-machine Google account) is preferred for authentication. 
+  Info: https://docs.gspread.org/en/v4.0.1/oauth2.html#for-bots-using-service-account
+  """
+  # `service_account.json` is in the same directory of this function
+  gc = gspread.service_account(filename='service_account.json')
+
+  # Get the key of an existing Google Sheet, obtainabled in the URL of the sheet: 
+  # Example: https://docs.google.com/spreadsheets/d/[KEY HERE]/edit#gid=111
+  sh = gc.open_by_key('foo')
+
+  # Get the email value. Set up additional logic here
+  worksheet = sh.get_worksheet(0)
+  email = worksheet.acell('A1').value
+  return email
+# [END main_gsheet(request)]
   
+# [START looker_user_provision]
 def looker_user_provision(email):
   user_id = search_users_by_email(email=email)
   if user_id is not None:
@@ -58,3 +102,4 @@ def create_users(email):
   # Send a welcome/setup email
   sdk.send_user_credentials_email_password_reset(user_id=new_user["id"])
   
+# [END looker_user_provision]
