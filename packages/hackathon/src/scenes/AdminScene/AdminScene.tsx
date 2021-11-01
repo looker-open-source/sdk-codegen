7e07abb46213b9c7fd3170f1d75788a621e6bbd8
jeremytchang <78522362+jeremytchang@users.noreply.github.com>
2021-11-01 10:13:01 -0700

    fix: Hackathon styling and resource update (#884)
    - Updated Loading component to default to always display if not
    passed in loading prop. Making it as dumb as possible
    - Updated Loading rendering logic and styling in scene components
    so layouts does not shift/change after loading is finished.
    - Updated side nav to be thinner to accomodate smaller viewports
    - Updated resources with new component and dataset tag
    - Added header to each page for easier navigability
    - Added hacker faq link to home/agenda page
    - Added additional text and field styling to resource page for
    better UX

--- packages/hackathon/src/scenes/AdminScene/AdminScene.tsx
+++ packages/hackathon/src/scenes/AdminScene/AdminScene.tsx
@@ -25,7 +25,14 @@
  */
 import type { FC } from 'react'
 import React, { useEffect } from 'react'
-import { TabList, Tab, TabPanels, TabPanel } from '@looker/components'
+import {
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Heading,
+  SpaceVertical,
+} from '@looker/components'
 import { useHistory, useRouteMatch } from 'react-router-dom'
 import { Routes } from '../../routes/AppRouter'
 import { getTabInfo } from '../../utils'
@@ -59,22 +66,30 @@ export const AdminScene: FC = () => {
 
   return (
     <>
-      <TabList selectedIndex={tabIndex} onSelectTab={onSelectTab}>
-        <Tab>General</Tab>
-        <Tab>Configuration</Tab>
-        <Tab>Add Users</Tab>
-      </TabList>
-      <TabPanels selectedIndex={tabIndex}>
-        <TabPanel>
-          <div>General admin stuff TBD</div>
-        </TabPanel>
-        <TabPanel>
-          <UserAttributes />
-        </TabPanel>
-        <TabPanel>
-          <AddUsers />
-        </TabPanel>
-      </TabPanels>
+      <Heading as="h2" fontSize="xxxlarge" fontWeight="medium">
+        Admin
+      </Heading>
+      {/* Tab components incorrectly sets content height causing unnecessary
+      scrolling. Wrapping with SpaceVertical fixes this. TODO: Remove this hack
+      once tab components are fixed. */}
+      <SpaceVertical gap="none">
+        <TabList selectedIndex={tabIndex} onSelectTab={onSelectTab}>
+          <Tab>General</Tab>
+          <Tab>Configuration</Tab>
+          <Tab>Add Users</Tab>
+        </TabList>
+        <TabPanels selectedIndex={tabIndex}>
+          <TabPanel>
+            <div>General admin stuff TBD</div>
+          </TabPanel>
+          <TabPanel>
+            <UserAttributes />
+          </TabPanel>
+          <TabPanel>
+            <AddUsers />
+          </TabPanel>
+        </TabPanels>
+      </SpaceVertical>
     </>
   )
 }
