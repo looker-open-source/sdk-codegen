ad08a679049db32c6b940201305d301a3c1662ef
John Kaster <kaster@google.com>
2021-12-05 20:48:25 +0000

    tested regen with Typescript

--- packages/sdk/src/3.1/models.ts
+++ packages/sdk/src/3.1/models.ts
@@ -50,7 +50,7 @@ export interface IAccessToken {
 }
 
 /**
- * The appropriate horizontal text alignment the values of this field should be displayed in. Valid values are: "left", "right".
+ * The appropriate horizontal text alignment the values of this field should be displayed in. Valid values are: "left", "right". (Enum defined in LookmlModelExploreField)
  */
 export enum Align {
   left = 'left',
@@ -143,7 +143,7 @@ export interface IBackupConfiguration {
 }
 
 /**
- * Field category Valid values are: "parameter", "filter", "measure", "dimension".
+ * Field category Valid values are: "parameter", "filter", "measure", "dimension". (Enum defined in LookmlModelExploreField)
  */
 export enum Category {
   parameter = 'parameter',
@@ -2111,7 +2111,7 @@ export interface IDependencyGraph {
 }
 
 /**
- * Status of the dependencies in your project. Valid values are: "lock_optional", "lock_required", "lock_error", "install_none".
+ * Status of the dependencies in your project. Valid values are: "lock_optional", "lock_required", "lock_error", "install_none". (Enum defined in ProjectWorkspace)
  */
 export enum DependencyStatus {
   lock_optional = 'lock_optional',
@@ -2361,7 +2361,7 @@ export interface IError {
 }
 
 /**
- * The style of dimension fill that is possible for this field. Null if no dimension fill is possible. Valid values are: "enumeration", "range".
+ * The style of dimension fill that is possible for this field. Null if no dimension fill is possible. Valid values are: "enumeration", "range". (Enum defined in LookmlModelExploreField)
  */
 export enum FillStyle {
   enumeration = 'enumeration',
@@ -2511,7 +2511,7 @@ export interface IFolderBase {
 }
 
 /**
- * Specifies the data format of the region information. Valid values are: "topojson", "vector_tile_region".
+ * Specifies the data format of the region information. Valid values are: "topojson", "vector_tile_region". (Enum defined in LookmlModelExploreFieldMapLayer)
  */
 export enum Format {
   topojson = 'topojson',
@@ -4856,7 +4856,7 @@ export interface IModelsNotValidated {
 }
 
 /**
- * The type of time interval this field represents a grouping of. Valid values are: "day", "hour", "minute", "second", "millisecond", "microsecond", "week", "month", "quarter", "year".
+ * The type of time interval this field represents a grouping of. Valid values are: "day", "hour", "minute", "second", "millisecond", "microsecond", "week", "month", "quarter", "year". (Enum defined in LookmlModelExploreFieldTimeInterval)
  */
 export enum Name {
   day = 'day',
@@ -5161,7 +5161,7 @@ export interface IPermissionSet {
 }
 
 /**
- * Type of permission: "view" or "edit" Valid values are: "view", "edit".
+ * Type of permission: "view" or "edit" Valid values are: "view", "edit". (Enum defined in ContentMetaGroupUser)
  */
 export enum PermissionType {
   view = 'view',
@@ -5417,7 +5417,7 @@ export interface IProjectWorkspace {
 }
 
 /**
- * The git pull request policy for this project. Valid values are: "off", "links", "recommended", "required".
+ * The git pull request policy for this project. Valid values are: "off", "links", "recommended", "required". (Enum defined in Project)
  */
 export enum PullRequestMode {
   off = 'off',
@@ -7376,7 +7376,7 @@ export interface IRequestUserRoles {
 }
 
 /**
- * Desired async query result format. Valid values are: "inline_json", "json", "json_detail", "json_fe", "csv", "html", "md", "txt", "xlsx", "gsxml".
+ * Desired async query result format. Valid values are: "inline_json", "json", "json_detail", "json_fe", "csv", "html", "md", "txt", "xlsx", "gsxml". (Enum defined in CreateQueryTask)
  */
 export enum ResultFormat {
   inline_json = 'inline_json',
@@ -8326,7 +8326,7 @@ export interface ISqlQueryCreate {
 }
 
 /**
- * A list of action types the integration supports. Valid values are: "cell", "query", "dashboard".
+ * A list of action types the integration supports. Valid values are: "cell", "query", "dashboard". (Enum defined in Integration)
  */
 export enum SupportedActionTypes {
   cell = 'cell',
@@ -8335,7 +8335,7 @@ export enum SupportedActionTypes {
 }
 
 /**
- * A list of all the download mechanisms the integration supports. The order of values is not significant: Looker will select the most appropriate supported download mechanism for a given query. The integration must ensure it can handle any of the mechanisms it claims to support. If unspecified, this defaults to all download setting values. Valid values are: "push", "url".
+ * A list of all the download mechanisms the integration supports. The order of values is not significant: Looker will select the most appropriate supported download mechanism for a given query. The integration must ensure it can handle any of the mechanisms it claims to support. If unspecified, this defaults to all download setting values. Valid values are: "push", "url". (Enum defined in Integration)
  */
 export enum SupportedDownloadSettings {
   push = 'push',
@@ -8343,7 +8343,7 @@ export enum SupportedDownloadSettings {
 }
 
 /**
- * A list of data formats the integration supports. If unspecified, the default is all data formats. Valid values are: "txt", "csv", "inline_json", "json", "json_label", "json_detail", "json_detail_lite_stream", "xlsx", "html", "wysiwyg_pdf", "assembled_pdf", "wysiwyg_png", "csv_zip".
+ * A list of data formats the integration supports. If unspecified, the default is all data formats. Valid values are: "txt", "csv", "inline_json", "json", "json_label", "json_detail", "json_detail_lite_stream", "xlsx", "html", "wysiwyg_pdf", "assembled_pdf", "wysiwyg_png", "csv_zip". (Enum defined in Integration)
  */
 export enum SupportedFormats {
   txt = 'txt',
@@ -8362,7 +8362,7 @@ export enum SupportedFormats {
 }
 
 /**
- * A list of formatting options the integration supports. If unspecified, defaults to all formats. Valid values are: "formatted", "unformatted".
+ * A list of formatting options the integration supports. If unspecified, defaults to all formats. Valid values are: "formatted", "unformatted". (Enum defined in Integration)
  */
 export enum SupportedFormattings {
   formatted = 'formatted',
@@ -8370,7 +8370,7 @@ export enum SupportedFormattings {
 }
 
 /**
- * A list of visualization formatting options the integration supports. If unspecified, defaults to all formats. Valid values are: "apply", "noapply".
+ * A list of visualization formatting options the integration supports. If unspecified, defaults to all formats. Valid values are: "apply", "noapply". (Enum defined in Integration)
  */
 export enum SupportedVisualizationFormattings {
   apply = 'apply',
@@ -8691,7 +8691,7 @@ export interface IUserAttribute {
 }
 
 /**
- * An array of user attribute types that are allowed to be used in filters on this field. Valid values are: "advanced_filter_string", "advanced_filter_number", "advanced_filter_datetime", "string", "number", "datetime", "relative_url", "yesno", "zipcode".
+ * An array of user attribute types that are allowed to be used in filters on this field. Valid values are: "advanced_filter_string", "advanced_filter_number", "advanced_filter_datetime", "string", "number", "datetime", "relative_url", "yesno", "zipcode". (Enum defined in LookmlModelExploreField)
  */
 export enum UserAttributeFilterTypes {
   advanced_filter_string = 'advanced_filter_string',
@@ -8902,7 +8902,7 @@ export interface IValidationErrorDetail {
 }
 
 /**
- * The name of the starting day of the week. Valid values are: "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday".
+ * The name of the starting day of the week. Valid values are: "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday". (Enum defined in LookmlModelExploreField)
  */
 export enum WeekStartDay {
   monday = 'monday',
