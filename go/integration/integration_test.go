9f2cc430e077da7a190ea768726e6b96d9589cfd
Jeremy Chang <jeremytchang@google.com>
2022-02-15 18:45:31 +0000

    wip

--- go/integration/integration_test.go
+++ go/integration/integration_test.go
@@ -1,45 +1,13 @@
 package integration
 
 import (
-	// "encoding/json"
-	// "io/ioutil"
-	// "path/filepath"
 	"testing"
-	// "strings"
-	// "fmt"
 
 	"github.com/looker-open-source/sdk-codegen/go/rtl"
 	v4 "github.com/looker-open-source/sdk-codegen/go/sdk/v4"
 )
 
-// type testDataStruct struct {
-// 	QueriesSystemActivity []v4.WriteQuery `json:"queries_system_activity"`
-// }
-
-// func getTestData(testData *testDataStruct) error {
-// 	absPath, err := filepath.Abs("../../test/data.yml.json")
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	fileContent, err := ioutil.ReadFile(absPath)
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return json.Unmarshal(fileContent, &testData)
-// }
-
 func TestIntegrationGoSDK(t *testing.T) {
-	// var testData testDataStruct
-	// err := getTestData(&testData)
-
-	// if err != nil {
-	// 	t.Errorf("Error reading/parsing test data to set up integration tests, error = %v", err)
-	// }
-
 	cfg, err := rtl.NewSettingsFromEnv()
 
 	if err != nil {
@@ -211,63 +179,5 @@ func TestIntegrationGoSDK(t *testing.T) {
 			t.Errorf("Session() does not return 'production' workspace id after UpdateSession(), got=%v", *session.WorkspaceId)
 		}
 	})
-
-	// t.Run("Create Run Query", func(t *testing.T) {
-	// 	query, err := sdk.CreateQuery(testData.QueriesSystemActivity[0], "", nil)
-
-	// 	if err != nil {
-	// 		t.Errorf("CreateQuery() failed. error=%v", err)
-	// 	}
-
-	// 	sql, err := sdk.RunQuery(v4.RequestRunQuery{
-	// 		QueryId: *query.Id,
-	// 		ResultFormat: "sql",
-	// 	}, nil)
-
-	// 	if err != nil {
-	// 		t.Errorf("RunQuery() failed. error=%v", err)
-	// 	}
-
-	// 	if !strings.Contains(sql, "SELECT") {
-	// 		t.Errorf("RunQuery() with 'sql' ResultFormat did not return string with 'SELECT'. got=%v", sql)
-	// 	}
-
-	// 	jsonString, err := sdk.RunQuery(v4.RequestRunQuery{
-	// 		QueryId: *query.Id,
-	// 		ResultFormat: "json",
-	// 	}, nil)
-
-	// 	if err != nil {
-	// 		t.Errorf("RunQuery() failed. error=%v", err)
-	// 	}
-
-	// 	fmt.Printf("jsonString %v \n", jsonString)
-
-	// 	// for q in queries_system_activity:
-    //     // limit = cast(str, q["limit"]) or "10"
-    //     // request = create_query_request(q, limit)
-    //     // query = sdk.create_query(request)
-    //     // assert isinstance(query, ml.Query)
-    //     // assert query.id
-    //     // assert isinstance(query.id, int)
-    //     // assert query.id > 0
-
-    //     // sql = sdk.run_query(query.id, "sql")
-    //     // assert "SELECT" in sql
-
-    //     // json_ = sdk.run_query(query.id, "json")
-    //     // assert isinstance(json_, str)
-    //     // json_ = json.loads(json_)
-    //     // assert isinstance(json_, list)
-    //     // assert len(json_) == int(limit)
-    //     // row = json_[0]
-    //     // if q.get("fields"):
-    //     //     for field in q["fields"]:
-    //     //         assert field in row.keys()
-
-    //     // csv = sdk.run_query(query.id, "csv")
-    //     // assert isinstance(csv, str)
-    //     // assert len(re.findall(r"\n", csv)) == int(limit) + 1
-	// })
 }
 
