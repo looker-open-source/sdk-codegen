425aee5097361b580b3ff7e93f227d6d34f21eb3
Bryn Ryans <bryans99@users.noreply.github.com>
2022-04-21 11:45:42 -0700

    feat: Add ExtensionProvider40 component (#1068)
    A simplified extension provider does not require a type or generic to identify
    the sdk required, basically it just gives access to Looker SDK 4.0.
    
    Also updates the playground package to demonstrate its use:
    
    1. packages updated.
    2. added call to me to demonstrate SDK 4.0 usage.
    3. now runs dev server on https.
    5. removed unnecessary hot HOC.

--- packages/extension-sdk-react/src/components/ExtensionProvider40/index.ts
+++ packages/extension-sdk-react/src/components/ExtensionProvider40/index.ts
@@ -2,7 +2,7 @@
 
  MIT License
 
- Copyright (c) 2021 Looker Data Sciences, Inc.
+ Copyright (c) 2022 Looker Data Sciences, Inc.
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -23,13 +23,5 @@
  SOFTWARE.
 
  */
-import React from 'react'
-import { ExtensionProvider40 } from '@looker/extension-sdk-react'
 
-import { Playground } from './Playground'
-
-export const App = () => (
-  <ExtensionProvider40>
-    <Playground />
-  </ExtensionProvider40>
-)
+export * from './ExtensionProvider40'
