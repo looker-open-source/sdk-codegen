5cd2046fc2742e1340cd06ae2814861172849407
John Kaster <kaster@google.com>
2022-09-21 00:06:30 +0000

    feat: generate SDKs for Looker 22.14

--- packages/sdk/src/4.0/streams.ts
+++ packages/sdk/src/4.0/streams.ts
@@ -2330,6 +2330,7 @@ export class Looker40SDKStream extends APIMethods {
         offset: request.offset,
         limit: request.limit,
         filter_or: request.filter_or,
+        permission: request.permission,
       },
       null,
       options
