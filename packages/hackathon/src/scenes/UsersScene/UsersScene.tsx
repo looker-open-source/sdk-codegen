88d509750dab943e72d9d1d3fa1878ab1adfef55
John Kaster <kaster@google.com>
2023-02-06 12:00:47 -0800

    linty fresh

--- packages/hackathon/src/scenes/UsersScene/UsersScene.tsx
+++ packages/hackathon/src/scenes/UsersScene/UsersScene.tsx
@@ -59,8 +59,9 @@ const tabnames = ['hackers', 'staff', 'judges', 'admins']
 
 export const UsersScene: FC<UsersSceneProps> = () => {
   const history = useHistory()
-  const match =
-    useRouteMatch<{ func: string; tabname: string }>('/:func/:tabname')
+  const match = useRouteMatch<{ func: string; tabname: string }>(
+    '/:func/:tabname'
+  )
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(allHackersRequest())
