7420e7a3bca75103a5e7fcc534893d87acf67490
jeremytchang <78522362+jeremytchang@users.noreply.github.com>
2023-03-28 11:47:13 -0700

    fix: Set Go SDK's User-Agent header (#1285)
    Implement AgentTag defined in settings and set Go SDK's outgoing Do requests with the User-Agent header.
    
    Fixes issue: #1274
    
    ---------
    
    Co-authored-by: Colin Gooding <cgooding@vendasta.com>

--- go/rtl/auth.go
+++ go/rtl/auth.go
@@ -128,8 +128,15 @@ func (s *AuthSession) Do(result interface{}, method, ver, path string, reqPars m
 		return err
 	}
 
-	// set content-type header
+	// set headers
 	req.Header.Add("Content-Type", contentTypeHeader)
+
+	if s.Config.AgentTag != "" {
+		req.Header.Set("User-Agent", s.Config.AgentTag)
+	}
+	if options != nil && options.AgentTag != "" {
+		req.Header.Set("User-Agent", options.AgentTag)
+	}
 
 	// set query params
 	setQuery(req.URL, reqPars)
