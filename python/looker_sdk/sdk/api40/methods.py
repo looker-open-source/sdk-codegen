fba2a3a7cee7863a3765c6bb40d8a897063ef225
Dr. Strangelove <drstrangelove@google.com>
2024-07-15 17:49:50 -0400

    feat: generate SDKs for Looker 24.12 (#1483)
    Release-As: 24.12.0

--- python/looker_sdk/sdk/api40/methods.py
+++ python/looker_sdk/sdk/api40/methods.py
@@ -819,8 +819,11 @@ class Looker40SDK(api_methods.APIMethods):
     #
     # This function does not strictly require all group_ids, user attribute names, or model names to exist at the moment the
     # embed url is created. Unknown group_id, user attribute names or model names will be passed through to the output URL.
+    # Because of this, **these parameters are not validated** when the API call is made.
     #
-    # To diagnose potential problems with an SSO embed URL, you can copy the signed URL into the Embed URI Validator text box in `<your looker instance>/admin/embed`.
+    # The [Get Embed Url](https://cloud.google.com/looker/docs/r/get-signed-url) dialog can be used to determine and validate the correct permissions for signing an embed url.
+    # This dialog also provides the SDK syntax for the API call to make. Alternatively, you can copy the signed URL into the Embed URI Validator text box
+    # in `<your looker instance>/admin/embed` to diagnose potential problems.
     #
     # The `secret_id` parameter is optional. If specified, its value must be the id of an active secret defined in the Looker instance.
     # if not specified, the URL will be signed using the most recent active signing secret. If there is no active secret for signing embed urls,
@@ -3236,6 +3239,7 @@ class Looker40SDK(api_methods.APIMethods):
     #  - data_connector_default_enabled
     #  - extension_framework_enabled
     #  - extension_load_url_enabled
+    #  - instance_config
     #  - marketplace_auto_install_enabled
     #  - marketplace_automation
     #  - marketplace_terms_accepted
@@ -3277,6 +3281,7 @@ class Looker40SDK(api_methods.APIMethods):
     #  - data_connector_default_enabled
     #  - extension_framework_enabled
     #  - extension_load_url_enabled
+    #  - instance_config
     #  - marketplace_auto_install_enabled
     #  - marketplace_automation
     #  - marketplace_terms_accepted
