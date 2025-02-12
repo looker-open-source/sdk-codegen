20c2d54cc87f1635330be75a8a0cc509a9bd0eed
John Kaster <kaster@google.com>
2025-02-11 16:07:12 -0800

    fix import issue

--- packages/sdk-rtl/src/oauthSession.spec.ts
+++ packages/sdk-rtl/src/oauthSession.spec.ts
@@ -24,7 +24,7 @@
 
  */
 
-import type { ArgValues } from '..';
+import type { ArgValues } from './constants';
 import type { ICryptoHash } from './cryptoHash';
 import { OAuthSession } from './oauthSession';
 import type { IApiSection, IApiSettings } from './apiSettings';
